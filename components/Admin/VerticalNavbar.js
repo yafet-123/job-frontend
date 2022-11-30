80b8dd5280490c8d08b1da79e97eaa68de7ff767
yafet-123 <yafetaddisu123@gmail.com>
2022-11-30 00:24:43 -0800

    fixing the sidebar of the admin page

--- components/Admin/VerticalNavbar.js
+++ components/Admin/VerticalNavbar.js
@@ -53,12 +53,17 @@ export function VerticalNavbar(){
                         </ul>
                     </div>
                     <div className={`mt-auto flex ${sideBar ? "flex-col" : "flex-col"}`}>
-                        <a className="flex items-center p-4 text-gray-300 hover:bg-gray-800 rounded-xl" href="#">
-                            <span className="mx-4 text-lg font-semibold">Settings</span>
-                        </a>
-                        <a className="flex items-center p-4 text-gray-300 hover:bg-gray-800 rounded-xl" href="#">
-                            <span className="mx-4 text-lg font-semibold">Log Out</span>
-                        </a>
+                        <Link  href="/">
+                            <a className="flex items-center p-4 text-gray-300 hover:bg-gray-800 rounded-xl" href="#">
+                                <span className="mx-4 text-lg font-semibold">Settings</span>
+                            </a>
+                        </Link>
+
+                        <Link  href="/">
+                            <a className="flex items-center p-4 text-gray-300 hover:bg-gray-800 rounded-xl" href="#">
+                                <span className="mx-4 text-lg font-semibold">Log Out</span>
+                            </a>
+                        </Link>
                     </div>
                 </nav>
             </div>
