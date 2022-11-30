bcfdde355b859f97b1995aa80a46c3b8124baf7c
yafet-123 <yafetaddisu123@gmail.com>
2022-11-30 15:14:50 -0800

    adding the admimn page dropsown

--- components/Admin/DashBoard.js
+++ components/Admin/DashBoard.js
@@ -1,36 +1,37 @@
 import React from "react";
+import { useState,useEffect, useContext} from 'react'
+
 export function DashBoard() {
+    const [getSearchValue,setgetSearchValue] = useState("")
+    const SearchList = [
+        { type: 1, name: "User",},
+        { type: 2, name: "Job",},
+        { type: 3, name: "Category",},
+    ];
     return (
         <div className="flex items-center justify-center mt-10">
             <input 
-                class="shadow appearance-none border rounded w-1/2 py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                className="shadow appearance-none border rounded w-5/12 py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
                 id="username" 
                 type="text"
-                placeholder="Search by"
+                placeholder="Search"
             />
-            <button 
-                id="dropdownDefault" 
-                data-dropdown-toggle="dropdown" 
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-4 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button"
-            >
-                Dropdown button 
-                <svg class="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
-            </button>
-            <div id="dropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700">
-                <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
-                  <li>
-                    <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                  </li>
-                  <li>
-                    <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                  </li>
-                  <li>
-                    <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                  </li>
-                  <li>
-                    <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                  </li>
-                </ul>
+            <div className="">
+                <div className="dropdown inline-block relative">
+                    <button className="w-32 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-4 text-center inline-flex items-center">
+                        <span className="mr-1">Search</span>
+                        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/> </svg>
+                    </button>
+                    <ul className="dropdown-menu absolute hidden text-gray-700 pt-1">
+                        {SearchList.map((search, index) => (
+                            <li className="">
+                                <button className="text-left w-32 bg-gray-200 hover:bg-gray-400 py-2 px-2">
+                                    By {search.name}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
   );
