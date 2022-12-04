8f38d56e6c8fee113057aaecdaa2e4acec5b9033
yafet-123 <yafetaddisu123@gmail.com>
2022-12-04 06:20:48 -0800

    display user add

--- components/Admin/DashBoard.js
+++ components/Admin/DashBoard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState,useEffect, useContext} from 'react'
+import axios from 'axios';
 
 export function DashBoard() {
     const [getSearchValue,setgetSearchValue] = useState("")
@@ -7,7 +8,24 @@ export function DashBoard() {
         { type: 1, name: "User",},
         { type: 2, name: "Job",},
         { type: 3, name: "Category",},
+        { type: 4, name: "Location",},
     ];
+
+    // const searchaxios = axios.create({
+    //     baseURL : api,
+    // })
+    
+
+    async function handleSearch(e){
+        const data = await axios.post(`api/searchPatient`,{
+            "searchName": getSearchValue,
+            "type": e
+        }).then(function (response) {
+            console.log(response.data);
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
     return (
         <div className="">
             <div className="flex items-center justify-center mt-10">
@@ -16,6 +34,8 @@ export function DashBoard() {
                     id="username" 
                     type="text"
                     placeholder="Search"
+                    value={getSearchValue}
+                    onChange={(e) => setgetSearchValue(e.target.value)}
                 />
                 <div className="">
                     <div className="dropdown inline-block relative">
@@ -26,7 +46,7 @@ export function DashBoard() {
                         <ul className="dropdown-menu absolute hidden text-black pt-1">
                             {SearchList.map((search, index) => (
                                 <li className="" key={index}>
-                                    <button className="text-left text-xl w-32 bg-white hover:bg-gray-400 py-2 px-2">
+                                    <button onClick={()=> handleSearch(search.type)} className="text-left text-xl w-32 bg-white hover:bg-gray-400 py-2 px-2">
                                         By {search.name}
                                     </button>
                                 </li>
--- pages/api/searchPatient.js
+++ pages/api/searchPatient.js
@@ -2,14 +2,92 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async(req, res) => {
-    const { searchName, type } = JSON.Parse(req.body)
+    const { searchName, type } = req.body
+    console.log(req.body)
     if (type == 1) {
-        const searchData = await prisma.User.findUnique({
+        const searchData = await prisma.User.findMany({
             where: {
-                id: Number(searchName),
+                UserName: {
+                    contains: searchName,
+                    mode: "insensitive",
+                },
+            },
+        })
+
+        const AllData = searchData.map((data)=>({
+            user_id:data.user_id,
+            UserName:data.UserName,
+            CreatedDate:data.CreatedDate,
+            ModifiedDate:data.ModifiedDate
+        }))
+
+        res.json(AllData)
+
+    } else if(type == 2){
+        const searchData = await prisma.Category.findMany({
+            where: {
+                CategoryName: {
+                    contains: searchName,
+                    mode: "insensitive",
+                },
+            },
+            include:{
+                User:{
+                    select:{
+                        UserName:true
+                    }
+                }
             }
         })
-    }
 
-    res.json(searchData)
+        const AllData = searchData.map((data)=>({
+            category_id:data.category_id,
+            CategoryName:data.CategoryName,
+            CreatedDate:data.CreatedDate,
+            ModifiedDate:data.ModifiedDate,
+            userName:data.User.UserName
+        }))
+
+        res.json(AllData)
+    } else if(type == 3){
+        const searchData = await prisma.Job.findMany({
+            where: {
+                CompanyName: {
+                    contains: searchName,
+                    mode: "insensitive",
+                },
+            },
+            include:{
+                User:{
+                    select:{
+                        UserName:true
+                    }
+                },
+                Category:{
+                    select:{
+                        CategoryName:true
+                    }
+                }
+            }
+        })
+
+        const AllData = searchData.map((data)=>({
+            job_id:data.job_id,
+            CompanyName:data.CompanyName,
+            JobsType:data.JobsType,
+            CareerLevel:data.CareerLevel,
+            EmploymentType:data.EmploymentType,
+            Salary:data.Salary,
+            DeadLine:data.DeadLine,
+            Apply:data.Apply,
+            CreatedDate:data.CreatedDate,
+            ModifiedDate:data.ModifiedDate,
+            userName:data.User.UserName
+            CategoryName:data.Category.CategoryName
+        }))
+
+        res.json(AllData)
+    } else if (type == 4)
+
+
 }
