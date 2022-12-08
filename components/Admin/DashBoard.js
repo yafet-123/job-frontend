c1a2b5194e0df02798918985f11a3e242b3b927d
yafet-123 <yafetaddisu123@gmail.com>
2022-12-08 14:25:56 -0800

    adding search value

--- components/Admin/DashBoard.js
+++ components/Admin/DashBoard.js
@@ -9,8 +9,8 @@ export function DashBoard() {
     const [type,settype] = useState()
     const SearchList = [
         { type: 1, name: "User",},
-        { type: 2, name: "Job",},
-        { type: 3, name: "Category",},
+        { type: 2, name: "Category",},
+        { type: 3, name: "Job",},
         { type: 4, name: "Location",},
     ];
 
@@ -101,6 +101,76 @@ export function DashBoard() {
                                     </td>
                                     <td className="p-3 text-lg text-gray-700 dark:text-white whitespace-nowrap">
                                         {moment(data.ModifiedDate).utc().format('YYYY-MM-DD')}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 md:hidden">
+                    {searchValue.map((data,index)=>(
+                        <div class="bg-white dark:bg-slate-800 space-y-3 p-4 rounded-lg shadow">
+                            <div class="flex items-center justify-between text-sm">
+                              <div>
+                                <p class="text-lg text-blue-500 dark:text-white font-bold hover:underline">{data.user_id}</p>
+                              </div>
+                              <div class="text-lg text-gray-700 dark:text-white font-bold">
+                                User Name : {data.UserName}
+                              </div>
+                            </div>
+                            <div class="text-sm text-black dark:text-white">
+                              createDate : {moment(data.createDate).utc().format('YYYY-MM-DD')}
+                            </div>
+                            <div class="text-sm text-black dark:text-white">
+                              Modified Date : {moment(data.ModifiedDate).utc().format('YYYY-MM-DD')}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div> }
+
+            { type == 2 && <div>
+                <div className="overflow-auto rounded-lg shadow hidden md:block">
+                    <table className="w-full">
+                        <thead className="bg-gray-50 dark:bg-slate-800 border-b-2 border-gray-200">
+                            <tr>
+                              <th className="text-black dark:text-white p-3 text-lg font-semibold tracking-wide text-left">Id</th>
+                              <th className="text-black dark:text-white p-3 text-lg font-semibold tracking-wide text-left">Category Name</th>
+                              <th className="text-black dark:text-white p-3 text-lg font-semibold tracking-wide text-left">Created Date</th>
+                              <th className="text-black dark:text-white p-3 text-lg font-semibold tracking-wide text-left">Modified Date</th>
+                              <th className="text-black dark:text-white p-3 text-lg font-semibold tracking-wide text-left">Created By</th>
+                            </tr>
+                        </thead>
+                        <tbody className="divide-y divide-gray-100">
+                            {searchValue.map((data,index)=>(
+                                <tr key={index} className="bg-white dark:bg-slate-900">
+                                    <td className="p-3 text-lg text-gray-700 whitespace-nowrap">
+                                        <p className="font-bold text-blue-500 dark:text-white hover:underline">{data.category_id}</p>
+                                    </td>
+                                    <td className="p-3 text-lg text-gray-700 dark:text-white whitespace-nowrap">
+                                        {data.CategoryName}
+                                    </td>
+                                    <td className="p-3 text-lg text-gray-700 dark:text-white whitespace-nowrap">
+                                        {moment(data.createDate).utc().format('YYYY-MM-DD')}
+                                    </td>
+                                    <td className="p-3 text-lg text-gray-700 dark:text-white whitespace-nowrap">
+                                        {moment(data.ModifiedDate).utc().format('YYYY-MM-DD')}
+                                    </td>
+
+                                    <td className="p-3 text-lg text-gray-700 dark:text-white whitespace-nowrap">
+                                        {data.userName}
+                                    </td>
+
+                                    <td className="p-3 text-lg text-gray-700 dark:text-white whitespace-nowrap">
+                                        <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                                            Edit
+                                        </button>
+                                    </td>
+
+                                    <td className="p-3 text-lg text-gray-700 dark:text-white whitespace-nowrap">
+                                        <button className="bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded">
+                                            Delete
+                                        </button>
                                     </td>
                                 </tr>
                             ))}
