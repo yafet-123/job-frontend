51abc4c18dda4bb744e87960d51eab14f3ae9cfd
yafet-123 <yafetaddisu123@gmail.com>
2022-12-14 12:38:08 -0800

    adding inseting job page

--- components/Admin/AddJob.js
+++ components/Admin/AddJob.js
@@ -4,7 +4,7 @@ import Multiselect from 'multiselect-react-dropdown';
 import DatePicker from "react-datepicker";
 
 export function AddJob({categories}) {
-    console.log(categories)
+    const [typechange , settypechange] = useState(true)
     const [CompanyName, setCompanyName] = useState("")
     const [JobsType, setJobsType] = useState("")
     const [Location, setLocation] = useState("")
@@ -16,9 +16,10 @@ export function AddJob({categories}) {
     const [startDate, setStartDate] = useState(new Date());
     const [Description , setDescription] = useState("")
     const [Requirement , setRequirement] = useState("")
-    console.log(categoryId)
+    console.log(typechange)
+
     return (
-        <div className="max-w-7xl mx-auto mt-10">
+        <div className="max-w-7xl mx-auto mt-10 h-full pb-10 mx-10">
             <h1 className="text-black dark:text-white text-4xl font-bold text-center italic">Add Job</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
                 <div className="relative mb-5">
@@ -134,6 +135,23 @@ export function AddJob({categories}) {
                         Apply
                     </label>
                 </div>
+
+                <div className="relative mb-5">
+                    <input 
+                        id="Apply" 
+                        type={typechange ? "text" : "date"} 
+                        className="block w-full px-3 text-xl text-black bg-transparent py-4 border-2 border-black rounded-xl appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-500 peer" placeholder=" "
+                        value={Apply}
+                        onChange={(e) => setApply(e.target.value)}
+                        onClick = {()=> settypechange(false)}
+                    />
+                    <label 
+                        htmlFor="floating_outlined" 
+                        className="absolute text-xl text-black dark:text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-200 dark:bg-slate-700 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+                    >
+                        DeadLine
+                    </label>
+                </div>
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-5 my-10">
@@ -148,7 +166,7 @@ export function AddJob({categories}) {
                     />
                     <label 
                         htmlFor="floating_outlined" 
-                        className="absolute text-xl text-black dark:text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-200 dark:bg-slate-700 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+                        className="absolute text-xl text-black dark:text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-200 dark:bg-slate-700 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-10 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
                     >
                         Job Description
                     </label>
@@ -165,7 +183,7 @@ export function AddJob({categories}) {
                     />
                     <label 
                         htmlFor="floating_outlined" 
-                        className="absolute text-xl text-black dark:text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-200 dark:bg-slate-700 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+                        className="absolute text-xl text-black dark:text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-200 dark:bg-slate-700 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-10 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
                     >
                         Jobs Requirement
                     </label>
@@ -196,7 +214,7 @@ export function AddJob({categories}) {
                         className="flex flex-col items-center justify-center w-full h-64 border-2 border-black border rounded-lg cursor-pointer bg-transparent dark:hover:bg-bray-800 dark:bg-slate-700 hover:bg-gray-100 dark:border-black dark:hover:border-gray-500 dark:hover:bg-gray-600"
                     >
                         <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                            <p className="text-lg text-black dark:text-white mb-5">Upload Jobs Descreption</p>
+                            <p className="text-lg text-black dark:text-white mb-5">Upload Company Image</p>
                             <svg aria-hidden="true" className="w-10 h-10 mb-3 text-black dark:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
                             <p className="mb-2 text-sm text-black dark:text-white"><span className="font-semibold">Click to upload</span> or drag and drop</p>
                         </div>
@@ -206,7 +224,7 @@ export function AddJob({categories}) {
             </div>
 
             <button 
-                className="float-right text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium text-xl rounded-lg px-4 py-4 text-center inline-flex items-center"
+                className="mb-10 float-right text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium text-xl rounded-lg px-4 py-4 text-center inline-flex items-center"
             >
                 Submit
             </button>
--- components/Admin/AddUser.js
+++ components/Admin/AddUser.js
@@ -22,7 +22,7 @@ export function AddUser({users}) {
     }
 
     return (
-        <div className="">
+        <div className="mx-10">
             <div className="max-w-7xl mx-auto mt-10">
                 <h1 className="text-black dark:text-white text-4xl font-bold text-center italic">User</h1>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-10 my-10 mx-2">
