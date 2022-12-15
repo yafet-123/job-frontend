c28fed0e30efcbac453a1b555cf6b21914d12642
yafet-123 <yafetaddisu123@gmail.com>
2022-12-15 11:34:18 -0800

    adding add job page

--- components/Admin/AddJob.js
+++ components/Admin/AddJob.js
@@ -145,7 +145,7 @@ export function AddJob({categories}) {
                     </div>
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-5 my-10">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-5 my-10">
                     <div className="relative mb-5">
                         <input 
                             id="Apply" 
@@ -159,23 +159,6 @@ export function AddJob({categories}) {
                             className="absolute text-xl text-black dark:text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-200 dark:bg-slate-700 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
                         >
                             Apply
-                        </label>
-                    </div>
-
-                    <div className="relative mb-5">
-                        <input 
-                            id="DeadLine" 
-                            type={typechange ? "text" : "date"} 
-                            className="block w-full px-3 text-xl text-black bg-transparent py-4 border-2 border-black rounded-xl appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-500 peer" placeholder=" "
-                            value={DeadLine}
-                            onChange={(e) => setDeadLine(e.target.value)}
-                            onClick = {()=> settypechange(false)}
-                        />
-                        <label 
-                            htmlFor="floating_outlined" 
-                            className="absolute text-xl text-black dark:text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-200 dark:bg-slate-700 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
-                        >
-                            DeadLine
                         </label>
                     </div>
 
