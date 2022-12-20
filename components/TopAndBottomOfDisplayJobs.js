e9a53ea87f13d724333244b8c032d667b8f0fa6c
yafet-123 <yafetaddisu123@gmail.com>
2022-12-20 13:58:40 -0800

    adding joe edit

--- components/TopAndBottomOfDisplayJobs.js
+++ components/TopAndBottomOfDisplayJobs.js
@@ -1,19 +1,11 @@
 import React from "react";
 import Link from "next/link";
-import {
-  FaFacebookF,
-  FaLinkedinIn,
-  FaTwitter,
-  FaYoutube,
-  FaTelegramPlane
-} from "react-icons/fa";
-export function TopAndBottomOfDisplayJobs() {
+import { FaFacebookF, FaLinkedinIn, FaTwitter, FaYoutube, FaTelegramPlane} from "react-icons/fa";
+import moment from 'moment';
+
+export function TopAndBottomOfDisplayJobs({DeadLine,Apply}) {
    const socialMediaLinks = [
-    {
-      path: "",
-      icon: <FaFacebookF />,
-      style: "bg-blue-900",
-    },
+    { path: "", icon: <FaFacebookF />, style: "bg-blue-900",},
     { path: "", icon: <FaTwitter />, style: "bg-blue-500" },
     { path: "", icon: <FaLinkedinIn />, style: "bg-blue-700" },
     { path: "", icon: <FaYoutube />, style: "bg-red-500" },
@@ -25,12 +17,12 @@ export function TopAndBottomOfDisplayJobs() {
     		<h1 className="px-10 py-3 bg-yellow-400 text-white border rounded-lg text-xl font-bold mr-5 ">Apply Now</h1>
     		<div className="flex flex-col text-lg text-red-700 font-bold">
     			<p className="">Deadline</p>
-    			<p className="">Nov 10, 2022</p>
+    			<p className="">{moment(DeadLine).utc().format('YYYY-MM-DD')}</p>
     		</div>
       	</div>
 
       	<div className="flex flex-col lg:flex-row justify-between items-center mb-10">
-      		<h1 className="text-black border rounded-lg text-4xl font-bold mr-5 mb-5 lg:mb-0">Share On</h1>
+      		<h1 className="text-black border rounded-lg text-2xl font-bold mr-5 mb-5 lg:mb-0">Share On</h1>
       		<div className="flex items-center">
                 {socialMediaLinks.map((main, index) => (
                   <Link key={index} href={main.path}>
