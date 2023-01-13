bceccd23841b03bfdcb23f6a1f51c8f797ced15f
yafet-123 <yafetaddisu123@gmail.com>
2023-01-13 03:49:28 -0800

    adding the header

--- components/AboutUsDetail.js
+++ components/AboutUsDetail.js
@@ -2,44 +2,138 @@ import Image from 'next/future/image';
 import AboutUsHeroImageOne from '../public/images/AboutUsImage.jpg';
 import AboutUsHeroImageTwo from '../public/images/AboutUsImage.jpg';
 import AboutUsHeroImageThree from '../public/images/AboutUsImage.jpg';
+import React from 'react';
 
 export const AboutUsDetail = () => {
   return (
-    <section className="mx-0 lg:mx-20 py-10">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 my-10 mx-5">
-        <div className="flex flex-col my-5 lg:my-10">
-          <h3 className="w-full text-xl lg:text-4xl text-center lg:text-left font-sans leading-8 font-bold text-[#165248] hover:text-[#94BB3C] mb-5 lg:mb-10">
-            We are Hulu Media
-          </h3>
-          <p className="mt-5 lg:mt-20 w-full lg:w-3/4 text-sm lg:text-2xl text-center lg:text-left font-sans leading-8 font-bold text-[#165248] hover:text-[#94BB3C]">
-            አንቀዕ፣ አንቀጽ
-          </p>
-        </div>
-
-        <div className="flex flex-col justify-between">
-          <Image
-            src={AboutUsHeroImageOne}
-            className="w-full h-[100px] object-cover brightness-75 border rounded-2xl"
-            priority
-            alt="AboutUs"
-          />
-
-          <div className="flex justify-between items-center mt-10">
+    <React.Fragment>
+      <div className="grid md:grid-cols-2 grid-cols-1 md:gap-16 gap-8 md:my-32 my-16 md:px-16 px-4">
+        <div className="flex flex-col justify-around gap-4">
+          <div>
             <Image
-              src={AboutUsHeroImageTwo}
-              className="w-[45%] h-[150px] lg:h-[223px] object-fit lg:object-cover brightness-75 border rounded-2xl mr-[5%]"
+              src={AboutUsHeroImageOne}
+              alt="sustainablity background"
+              //   fill
               priority
-              alt="AboutUs"
-            />
-            <Image
-              src={AboutUsHeroImageThree}
-              className="w-[40%] h-[150px] lg:h-[223px] object-fit lg:object-cover brightness-75 border rounded-2xl ml-[5%]"
-              priority
-              alt="AboutUs"
+              width={500}
+              height={200}
+              className="md:w-full rounded-lg shadow-md shadow-slate-800"
             />
           </div>
         </div>
+        <div className="flex flex-col text-left justify-center gap-4 bg-white text-[#165248]">
+          <h1 className="md:text-5xl font-bold text-3xl">
+            We are Eco-Travel Ethiopia
+          </h1>
+          <p className="md:text-2xl font-semibold text-md md:text-lg">
+            We organize the best Ethiopian premium tours covering historical
+            routes, mountain trekking and adventure expeditions to remote areas,
+            bird and wildlife watching, as well as family-friendly tours.
+          </p>
+          {/* <div className="justify-self-end">
+            <Image
+              src="https://jonesbrotherscoffee.com/en/img/asset/bWFpbi9ibG9nL2ltYWdlLWFzc2V0LmpwZw==?w=1200&s=f14631dd1dec01f2a90d6ce25bf50438"
+              alt="sustainablity background"
+              //   fill
+              priority
+              width={500}
+              height={400}
+              className="md:w-full md:h-full rounded-lg shadow-md shadow-slate-800"
+            />
+          </div> */}
+        </div>
       </div>
-    </section>
+
+      <div className="flex flex-col gap-8 ">
+        <div className="my-16 grid md:grid-cols-2 grid-cols-1 items-center bg-[#165248] justify-center md:p-20 md:mt-12 md:mb-32 gap-4 p-2 text-white text-left">
+          <div className="order-first md:order-last md:-mb-40">
+            <Image
+              src={AboutUsHeroImageOne}
+              alt="sustainablity background"
+              //   fill
+              priority
+              width={500}
+              height={400}
+              className="md:h-full md:w-auto rounded-sm shadow-md shadow-slate-800"
+            />
+          </div>
+
+          <div className="flex gap-8 flex-col">
+            <h1 className="md:text-5xl font-bold text-4xl mt-6">OUR MISSION</h1>
+            <p className="md:text-2xl md:font-semibold text-md md:text-lg">
+              Our mission is to provide the ultimate travel planning experience
+              while becoming a one-stop shop for every travel service available
+              in the industry.
+            </p>
+          </div>
+        </div>
+
+        <div className="grid md:grid-cols-2 grid-cols-1 items-center justify-start md:mt-32 gap-4 md:p-20 p-4 text-white p-2 text-left bg-[#165248]">
+          <div className="order-last md:order-first md:-mt-40 mt-0">
+            <Image
+              src={AboutUsHeroImageOne}
+              alt="sustainablity background"
+              //   fill
+              priority
+              width={500}
+              height={400}
+              className="md:h-full md:w-auto rounded-sm shadow-md shadow-slate-800"
+            />
+          </div>
+
+          <div className="flex gap-8 flex-col">
+            <h1 className="md:text-5xl font-bold text-4xl mt-6">OUR VISION</h1>
+            <p className="md:text-2xl md:font-semibold text-md md:text-lg">
+              We are proud to offer excellent quality and value for money in our
+              tours, which give you the chance to experience your chosen
+              destination in an authentic and exciting way.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-around items-center bg-[#165248] my-16">
+        <h1 className="md:text-5xl font-bold text-4xl text-center text-white mt-6">
+          Following the Dream
+        </h1>
+        <div className="flex md:flex-row flex-col justify-between gap-8 m-8">
+          {FollowingTheDreamData.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className="flex flex-col items-center text-center bg-white gap-4 p-10 hover:shadow-2xl rounded-xl shadow-lg shadow-slate-800"
+              >
+                <h1 className="md:text-6xl font-bold text-5xl text-[#165248]">
+                  {item.numbers}+
+                </h1>
+                <h1 className="md:text-3xl font-bold text-2xl text-[#165248]">
+                  {item.title}
+                </h1>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
+
+const FollowingTheDreamData = [
+  {
+    id: 1,
+    numbers: 200,
+    title: 'Happy Travelers',
+  },
+  {
+    id: 2,
+    numbers: 20,
+    title: 'Destinations',
+  },
+  {
+    id: 3,
+    numbers: 5,
+    title: 'Team Members',
+  },
+];
+
+
--- pages/About.js
+++ pages/About.js
@@ -9,9 +9,6 @@ export default function About() {
     <div className="flex flex-col">
       <AboutUsHero />
       <AboutUsDetail />
-      <FollowingTheDream />
-      <OurMission />
-      <OurVision />
     </div>
   );
 }
