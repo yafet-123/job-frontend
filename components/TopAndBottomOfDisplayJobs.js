e35b550a39f59ec5f5b97d570f7194b4a9174523
yafet-123 <yafetaddisu123@gmail.com>
2023-01-15 11:18:14 -0800

    adding hero page

--- components/TopAndBottomOfDisplayJobs.js
+++ components/TopAndBottomOfDisplayJobs.js
@@ -12,31 +12,31 @@ export function TopAndBottomOfDisplayJobs({DeadLine,Apply}) {
     { path: "", icon: <FaTelegramPlane />, style: "bg-blue-600" },
   ];
   return (
-    <div className="bg-gray-200 dark:bg-slate-700 flex flex-col lg:flex-row justify-between items-center w-full lg:h-28 border rounded-lg dark:border-slate-700 p-20">
-    	<div className="flex items-center mb-10">
-    		<h1 className="px-10 py-3 bg-yellow-400 text-white border rounded-lg text-xl font-bold mr-5 ">Apply Now</h1>
-    		<div className="flex flex-col text-lg text-red-700 font-bold">
+    <div className="bg-gray-200 dark:bg-slate-700 flex flex-col lg:flex-row justify-between items-center w-full lg:h-28 border rounded-lg dark:border-slate-700 px-0 lg:px-20 py-20">
+    	<div className="flex mb-10">
+    		<h1 className="px-3 lg:px-10 py-3 bg-yellow-400 text-white border rounded-lg text-md lg:text-xl font-bold mr-5 ">Apply Now</h1>
+    		<div className="flex flex-col text-sm lg:text-lg text-red-700 font-bold">
     			<p className="">Deadline</p>
     			<p className="">{moment(DeadLine).utc().format('YYYY-MM-DD')}</p>
     		</div>
-      	</div>
+      </div>
 
-      	<div className="flex flex-col lg:flex-row justify-between items-center mb-10">
-      		<h1 className="text-black dark:text-white border rounded-lg text-2xl font-bold mr-5 mb-5 lg:mb-0 dark:border-slate-700">Share On</h1>
-      		<div className="flex items-center">
-                {socialMediaLinks.map((main, index) => (
-                  <Link key={index} href={main.path}>
-                    <a target="_blank" rel="noreferrer">
-                      <div
-                        className={`${main.style} ml-3 text-white rounded-lg shadow-lg shadow-gray-400 p-3 hover:w-20 transition-all duration-1000 transform-cpu cursor-pointer hover:brightness-110 flex items-center justify-center`}
-                      >
-                        {main.icon}
-                      </div>
-                    </a>
-                  </Link>
-                ))}
-              </div>
-      	</div>
+      <div className="flex flex-col lg:flex-row justify-between items-center mb-10">
+      	<h1 className="text-black dark:text-white border rounded-lg text-2xl font-bold mr-5 mb-5 lg:mb-0 dark:border-slate-700">Share On</h1>
+      	<div className="flex items-center">
+          {socialMediaLinks.map((main, index) => (
+            <Link key={index} href={main.path}>
+              <a target="_blank" rel="noreferrer">
+                <div
+                  className={`${main.style} ml-3 text-white rounded-lg shadow-lg shadow-gray-400 p-3 hover:w-20 transition-all duration-1000 transform-cpu cursor-pointer hover:brightness-110 flex items-center justify-center`}
+                >
+                  {main.icon}
+                </div>
+              </a>
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- pages/DisplayJobs.js
+++ pages/DisplayJobs.js
@@ -80,62 +80,62 @@ export default function DisplayJobs({job, categories}) {
     <section className="flex flex-col w-full h-full px-0 md:px-32 bg-gray-200 dark:bg-slate-700 p-5">
       	<TopAndBottomOfDisplayJobs DeadLine={job.DeadLine} Apply={job.Apply}/>
       	<div className="flex flex-col bg-white p-5 pb-20 bg-white dark:bg-slate-600">
-      		<div className="flex justify-between items-center mt-10 mx-5">
+      		<div className="flex justify-between items-center mt-10 mx-0 lg:mx-5">
 		      	<div className="flex flex-col w-3/4">
-		      		<h1 className="text-black text-3xl capitalize font-bold mb-2 text-black dark:text-white">{job.JobsType}</h1>
-		      		<p className="text-lg font-bold w-3/4 text-gray-500 dark:text-gray-400">Job by {job.CompanyName}</p>
+		      		<h1 className="text-black text-md lg:text-3xl capitalize font-bold mb-2 text-black dark:text-white">{job.JobsType}</h1>
+		      		<p className="text-sm lg:text-lg font-bold w-3/4 text-gray-500 dark:text-gray-400">Job by {job.CompanyName}</p>
 		      	</div>
 
-		      	<div className="flex flex-col border rounded-xl text-black dark:text-white text-lg font-bold">
+		      	<div className="flex flex-col border text-black dark:text-white text-sm lg:text-lg font-bold">
 		      		<p className="bg-sky-500 text-center text-white">Posted</p>
 		      		<p className="p-5">{moment(job.ModifiedDate).utc().format('dddd, MMM Y')}</p>
 		      	</div>
       		</div>
 
-	      	<ul className="mx-20 mt-10 text-black dark:text-white">
+	      	<ul className="mt-10 text-black dark:text-white">
 	      		<li className="flex flex-row justify-between w-full mb-5">
-	      			<h1 className="text-xl font-bold capitalize text-left w-1/2">category:</h1>
-	      			<div className="grid grid-cols-2 gap-5 w-1/2">
+	      			<h1 className="text-md lg:text-xl font-bold capitalize text-left w-1/2">category:</h1>
+	      			<div className="grid grid-cols-1 lg:grid-cols-2 gap-5 w-1/2">
 		      			{ categories.map((data,index)=>(
-		      				<p key={index} className="text-lg text-left ">{data.CategoryName}</p>
+		      				<p key={index} className="text-xs lg:text-lg text-left ">{data.CategoryName}</p>
 		      			))}
 		      		</div>
 	      		</li>
 
 	      		<li className="flex flex-row justify-between w-full mb-5">
-	      			<h1 className="text-xl font-bold capitalize text-left w-1/2">Location:</h1>
-	      			<p className="text-lg text-left w-1/2">{job.Location}</p>
+	      			<h1 className="text-md lg:text-xl font-bold capitalize text-left w-1/2">Location:</h1>
+	      			<p className="text-xs lg:text-lg text-left w-1/2">{job.Location}</p>
 	      		</li>
 
 	      		<li className="flex flex-row justify-between w-full mb-5">
-	      			<h1 className="text-xl font-bold capitalize text-left w-1/2">Career Level:</h1>
-	      			<p className="text-lg text-left w-1/2">{job.CareerLevel}</p>
+	      			<h1 className="text-md lg:text-xl font-bold capitalize text-left w-1/2">Career Level:</h1>
+	      			<p className="text-xs lg:text-lg text-left w-1/2">{job.CareerLevel}</p>
 	      		</li>
 
 	      		<li className="flex flex-row justify-between w-full mb-5">
-	      			<h1 className="text-xl font-bold capitalize text-left w-1/2">Employment Type:</h1>
-	      			<p className="text-lg text-left w-1/2">{job.EmploymentType}</p>
+	      			<h1 className="text-md lg:text-xl font-bold capitalize text-left w-1/2">Employment Type:</h1>
+	      			<p className="text-xs lg:text-lg text-left w-1/2">{job.EmploymentType}</p>
 	      		</li>
 
 	      		<li className="flex flex-row justify-between w-full mb-5">
-	      			<h1 className="text-xl font-bold capitalize text-left w-1/2">Salary:</h1>
-	      			<p className="text-lg text-left w-1/2">{job.Salary}</p>
+	      			<h1 className="text-md lg:text-xl font-bold capitalize text-left w-1/2">Salary:</h1>
+	      			<p className="text-xs lg:text-lg text-left w-1/2">{job.Salary}</p>
 	      		</li>
 	      	</ul>
 
 	      	<div className="flex flex-col justify-between mt-10 mx-5 ">
-	      		<h1 className="text-blue-700 capitalize text-3xl font-bold mb-2">job descreption</h1>
-	      		<div dangerouslySetInnerHTML={{ __html: job.JobsDescreption }} />
+	      		<h1 className="text-blue-700 capitalize text-xl lg:text-3xl font-bold mb-2">job descreption</h1>
+	      		<div className="text-md lg:text-xl font-normal mb-5 h-36 overflow-hidden" dangerouslySetInnerHTML={{ __html: job.JobsDescreption }} />
 	      	</div>
 
 	      	<div className="flex flex-col justify-between mt-10 mx-5">
-	      		<h1 className="text-blue-700 capitalize text-3xl font-bold mb-2">job requirement</h1>
-	      		<div dangerouslySetInnerHTML={{ __html: job.JobsRequirement }} />
+	      		<h1 className="text-blue-700 capitalize text-xl lg:text-3xl font-bold mb-2">job requirement</h1>
+	      		<div className="text-md lg:text-xl font-normal mb-5 h-36 overflow-hidden" dangerouslySetInnerHTML={{ __html: job.JobsRequirement }} />
 	      	</div>
 
 	      	<div className="flex flex-col justify-between mt-10 mx-5">
-	      		<h1 className="text-blue-700 capitalize text-3xl font-bold mb-2">How to Apply</h1>
-	      		<div dangerouslySetInnerHTML={{ __html: job.Apply }} />
+	      		<h1 className="text-blue-700 capitalize text-xl lg:text-3xl font-bold mb-2">How to Apply</h1>
+	      		<div className="text-md lg:text-xl font-normal mb-5 h-36 overflow-hidden" dangerouslySetInnerHTML={{ __html: job.Apply }} />
 	      	</div>
       	</div>
       	<TopAndBottomOfDisplayJobs DeadLine={job.DeadLine} Apply={job.Apply}/>
