f2cf0a3f185dc2db64147fc86de3e9f3af31eece
yafet-123 <yafetaddisu123@gmail.com>
2023-01-16 14:55:06 -0800

    adding loding state

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,13 @@ import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
 import { ThemeProvider } from 'next-themes';
 import { SessionProvider } from "next-auth/react"
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
