3778936dc13f715be2bc2178bf192703aa01ba39
yafet-123 <yafetaddisu123@gmail.com>
2023-01-16 16:47:01 -0800

    adding loding state

--- pages/JobsByCategory.js
+++ pages/JobsByCategory.js
@@ -112,10 +112,10 @@ export default function JobsByCategory({categories,Alllatestjobs, jobsbycategory
   return (
     <section className="bg-gray-200 dark:bg-slate-700 flex flex-col w-full h-full py-20 px-0 md:px-32">
     	<div className="flex flex-col bg-white dark:bg-slate-800 w-full h-full px-5 py-10 border rounded-xl">
-	    	<h1 className="text-black dark:text-white text-3xl capitalize font-bold mb-10">{howmany} {category} Jobs</h1>
+	    	<h1 className="text-black dark:text-white text-xl lg:text-3xl capitalize font-bold mb-3 lg:mb-10">{howmany} {category} Jobs</h1>
       	<div className="flex flex-col md:flex-row w-full ">
       		<div className="flex flex-col w-full lg:w-1/4 bg-white p-3 dark:bg-slate-800">
-      			<h1 className="text-2xl text-black dark:text-white font-bold capitalize text-center mb-10">Jobs By Category</h1>
+      			<h1 className="text-md lg:text-2xl text-black dark:text-white font-bold capitalize text-center mb-10">Jobs By Category</h1>
       				<div className="flex flex-col h-96 lg:h-[40rem] overflow-y-scroll bg-gray-200 dark:bg-slate-700 p-3">
 	      				{categories.map((data, index) => (
 	      					<button 
@@ -202,13 +202,13 @@ export default function JobsByCategory({categories,Alllatestjobs, jobsbycategory
       			}
       		</div>
       		<div className="flex flex-col w-full lg:w-1/4 h-[45rem] p-3 border rounded-lg bg-white dark:bg-slate-800">
-      			<div className="flex justify-between items-center p-10 md:p-0">
-			        <div className="flex items-center font-bold text-xl text-black dark:text-white capitalize">
+      			<div className="flex justify-between items-center p-2 md:p-0">
+			        <div className="flex items-center font-bold text-md lg:text-xl text-black dark:text-white capitalize">
 			          <AiOutlineClockCircle size={20} />
 			          <span className="ml-5">Latest Jobs</span>
 			        </div>
 			        <Link href="">
-			          <a className="font-bold text-lg text-white p-4 bg-blue-700 capitalize border rounded-2xl">
+			          <a className="font-bold text-sm md:text-md lg:text-lg text-white p-2 lg:p-4 bg-blue-700 capitalize border rounded-2xl">
 			            view all jobs
 			          </a>
 			        </Link>
@@ -225,18 +225,18 @@ export default function JobsByCategory({categories,Alllatestjobs, jobsbycategory
 			          >
 			            <a className="flex justify-around items-center mb-5 even:bg-white even:dark:bg-slate-600 px-5 py-5 group">
 			              <div className="flex flex-col w-3/4">
-			                <h1 className="font-bold text-sm text-blue-500 group-hover:text-orange-500">
+			                <h1 className="text-left font-bold text-sm md:text-lg lg:text-xl text-blue-500 dark:text-white group-hover:text-orange-500">
 			                  {data.JobsType}
 			                </h1>
-			                <h1 className="font-light md:text-xs text-blue-500 group-hover:text-orange-500">
+			                <h1 className="text-left font-light text-xs md:text-sm lg:text-lg text-blue-500 dark:text-white group-hover:text-orange-500">
 			                  {data.CompanyName}
 			                </h1>
 			              </div>
 			              <div className="flex flex-col w-1/4">
-			                <h1 className="font-light text-sm text-blue-500 text-right group-hover:text-orange-500">
+			                <h1 className="font-light text-xs md:text-sm lg:text-lg text-blue-500 dark:text-white text-right group-hover:text-orange-500">
 			                  {moment(data.CreatedDate).utc().format('MMM DD YYYY')}
 			                </h1>
-			                <h1 className="font-light text-sm text-blue-500 text-right group-hover:text-orange-500">
+			                <h1 className="font-light text-xs md:text-sm lg:text-lg text-blue-500 dark:text-white text-right group-hover:text-orange-500">
 			                  {data.Location}
 			                </h1>
 			              </div>
