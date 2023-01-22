6eaed51777be70054467c79319fbf6bde3046756
yafet-123 <yafetaddisu123@gmail.com>
2023-01-22 13:50:50 -0800

    adding mobile view

--- components/Admin/DeleteUser.js
+++ components/Admin/DeleteUser.js
@@ -17,7 +17,7 @@ export function DeleteUser({setdeleteModalOn,deleteuserid}) {
 	return(
 		<div className="bg-gray-200 dark:bg-slate-800 opacity-90 fixed inset-0 z-50   ">
             <div className="flex h-screen justify-center items-center ">
-                <div className="flex-col justify-center bg-white dark:bg-slate-500 py-24 px-24 border-4 border-sky-500 rounded-xl ">
+                <div className="flex-col justify-center bg-white dark:bg-slate-500 py-24 px-5 lg:px-10 border-4 border-sky-500 rounded-xl ">
                     <div className="flex text-xl text-zinc-600 font-bold mb-10 dark:text-white" >Are you sure You want to delete User ?</div>
                     <div className="flex">
                     	<button onClick={handleOKClickFordelete} className=" rounded px-4 py-4 text-white  bg-green-400 hover:bg-green-600">Yes</button>
