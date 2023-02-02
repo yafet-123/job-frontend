106a37475360e1face522df110424c26b668eaae
yafet-123 <yafetaddisu123@gmail.com>
2023-02-02 07:05:19 -0800

    adding the first job

--- components/Admin/AddJob.js
+++ components/Admin/AddJob.js
@@ -40,7 +40,7 @@ export function AddJob({categories, locations}) {
             
         )
         imagesecureUrl = imageUpload.secure_url
-        return imagesecureUrls
+        return imagesecureUrl
     }
 
     async function addJobData(){
