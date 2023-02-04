0ddb2ccaa2a55955cb499afc7d9bbfef9d2196d8
yafet-123 <yafetaddisu123@gmail.com>
2023-02-04 09:34:20 -0800

    adding the news page

--- pages/News.js
+++ pages/News.js
@@ -6,6 +6,7 @@ import { MainHeader } from '../components/MainHeader';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import AboutUsImage from '../public/images/bgImage3.avif';
 
 export default function News() {
   var settings = {
@@ -18,30 +19,38 @@ export default function News() {
   return (
     <React.Fragment>
       <MainHeader title="News" />
-      <section className="flex flex-col w-full h-screen px-5 lg:px-24 bg-gray-200 dark:bg-slate-700 pt-32">
+      <section className="flex flex-col w-full h-full bg-white dark:bg-slate-700 py-32 px-56">
         <h1 className="text-center text-xl lg:text-3xl">Lates News</h1>
-        <div>
-          <Slider {...settings}>
-            <div>
-              <h3>1</h3>
-            </div>
-            <div>
-              <h3>2</h3>
-            </div>
-            <div>
-              <h3>3</h3>
-            </div>
-            <div>
-              <h3>4</h3>
-            </div>
-            <div>
-              <h3>5</h3>
-            </div>
-            <div>
-              <h3>6</h3>
-            </div>
-          </Slider>
-        </div>
+        <Slider {...settings}>
+          <div className="!flex flex-col lg:flex-row h-full pl-5">
+              <Image
+                src={AboutUsImage}
+                width={1000}
+                height={1000}
+                className="border border-black rounded-xl w-full"
+                alt="latest news image"
+              />
+
+              <div className="flex flex-col my-20 lg:mx-10">
+                <h3 className="mb-5">
+                  <span className="text-2xl font-bold text-black"> Category Name </span>
+                  <span className="font-normal text-lg text-gray-600">
+                     - Date of the post
+                  </span>
+                </h3>
+
+                <h1 className="text-xl lg:text-4xl font-extrabold text-black tracking-wide leading-snug lg:w-3/4">
+                  Your most unhappy customers are your greater source of learning.
+                </h1>
+
+                <p className="mt-5 leading-loose font-sans text-lg font-medium tracking-wide text-left text-slate-700">
+                  So I started to walk into the water. I won't lie to you boys, I was terrified. But I pressed on, and as I made my 
+                  way past the breakers a strange calm came over me. I don't know if it was divine intervention or the kinship of 
+                  all living things but I tell you Jerry at that moment, I was a marine biologist.
+                </p>
+              </div>
+          </div>
+        </Slider>
       </section>
     </React.Fragment>
   );
