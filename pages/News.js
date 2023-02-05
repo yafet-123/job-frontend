2912a872c91d00566e5fa1bd6f4c6c465a4b0394
yafet-123 <yafetaddisu123@gmail.com>
2023-02-05 08:14:46 -0800

    adding the latest news

--- pages/News.js
+++ pages/News.js
@@ -26,9 +26,9 @@ export default function News() {
   return (
     <React.Fragment>
       <MainHeader title="News" />
-      <section className="flex flex-col w-full h-full bg-white dark:!bg-slate-700 py-32">
-        <div className="max-w-7xl mx-auto">
-          <h1 className="text-center text-3xl lg:text-7xl font-bold my-10 italic">Latest News</h1>
+      <section className="w-full h-full bg-white dark:!bg-slate-700 py-32">
+        <div className="max-w-7xl mx-auto flex flex-col">
+          <h1 className="text-center text-3xl lg:text-7xl font-bold my-10 italic">Trending</h1>
           <Slider {...settings}>
             <div className="!flex flex-col lg:flex-row px-2 w-full h-full lg:h-96">
                 <div className="w-full lg:w-1/2 h-52 lg:!h-96">
@@ -153,6 +153,40 @@ export default function News() {
                 </div>
             </div>
           </Slider>
+
+          <div className="bg-white dark:bg-slate-700 py-5 px-2 lg:px-10 w-full h-full">      
+            <h1 className="text-center text-3xl lg:text-5xl font-bold my-5 italic">Latest News</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-10 mb-5 w-full h-full">
+              {NewsTemplate.map((data,index)=>(
+                <div key={index} className="flex flex-col w-full h-full lg:mt-20 float-right">
+                  <Image
+                    src={AboutUsImage4}
+                    layout="raw" 
+                    className="!bg-cover w-full !h-64 border rounded-xl"
+                    alt="latest news image"
+                  />
+                  <div className="w-full flex flex-col my-5">
+                    <h3 className="mb-5">
+                      <span className="text-md lg:text-lg font-bold dark:text-white text-black"> Category Name </span>
+                      <span className="font-normal text-sm lg:text-md dark:text-white text-gray-600">
+                         - Date of the post
+                      </span>
+                    </h3>
+
+                    <h1 className="text-lg lg:text-2xl font-extrabold dark:text-white text-black tracking-wide leading-snug">
+                      Your most customers are your greater source of learning.
+                    </h1>
+
+                    <p className="mt-5 leading-loose font-sans text-sm lg:text-md font-medium tracking-wide text-left dark:text-white text-slate-700">
+                      The component accepts a number of additional properties beyond those which are required. This section describes 
+                      the most commonly-used properties of the Image component. Find details about more rarely-used properties in the Advanced 
+                      Props section
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </section>
     </React.Fragment>
