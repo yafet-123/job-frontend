11807563c5579e2651202ef03a1948b57ba253e1
yafet-123 <yafetaddisu123@gmail.com>
2023-02-14 13:10:38 -0800

    fixing the error

--- pages/Jobs.js
+++ pages/Jobs.js
@@ -87,7 +87,7 @@ export default function Jobs({categories, locations, latestjobs}) {
   return (
     <React.Fragment>
       <MainHeader title="Jobs" />
-      <section className="flex flex-col w-full h-full py-20 px-0 md:px-24 bg-gray-200 dark:bg-slate-700 pt-32">
+      <section className="flex flex-col w-full h-full py-20 px-0 md:px-24 bg-neutral-300 dark:bg-slate-700 pt-32">
         <div className="flex flex-col lg:flex-row justify-between items-center h-16 w-full mb-10 px-10">
           <h1 className="text-black dark:text-white font-light text-md md:text-3xl lg:text-4xl capitalize lg:mb-5 text-center">
             Search and Find Jobs in Ethiopia
@@ -103,14 +103,14 @@ export default function Jobs({categories, locations, latestjobs}) {
     
         <hr className="w-full bg-gray-200 mb-5" />
 
-        <div className="flex w-full h-[25rem] lg:h-[45rem] bg-white dark:bg-slate-800 border rounded-xl p-2">
+        <div className="flex w-full h-[25rem] lg:h-[45rem] bg-neutral-100 dark:bg-slate-800 border rounded-xl p-2">
           <div className="flex-1 border rounded-xl shadow-2xl shadow-sky-200 border-slate-300 dark:border-slate-800 lg:p-5">
             <div className="w-full h-full overflow-y-scroll">
               {jobs == "latest" && (
                 <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-5 py-10">
                   {latestjobs.map((data, index) => (
                     <button
-                      className="flex justify-between items-center mb-3 group hover:bg-gray-200 px-2 lg:px-4 py-2 w-full"
+                      className="flex justify-between items-center mb-3 group hover:bg-neutral-500 px-2 lg:px-4 py-2 w-full"
                       key={index}
                       type = "button"
                       onClick = {()=>{
@@ -121,18 +121,18 @@ export default function Jobs({categories, locations, latestjobs}) {
                       }}
                     >
                       <div className="flex flex-col w-2/4 lg:w-3/4">
-                        <h1 className="font-normal text-sm lg:text-lg text-blue-500 dark:text-white group-hover:text-orange-500 text-left">
+                        <h1 className="font-normal text-sm lg:text-lg text-blue-500 dark:text-white group-hover:text-white text-left">
                           {data.JobsType}
                         </h1>
-                        <h1 className="font-light text-xs lg:text-sm text-black dark:text-white group-hover:text-orange-500 text-left">
+                        <h1 className="font-light text-xs lg:text-sm text-black dark:text-white group-hover:text-white text-left">
                           {data.CompanyName}
                         </h1>
                       </div>
                       <div className="flex flex-col items-center justify-center w-1/4 lg:w-1/4">
-                        <h1 className="font-light text-xs text-black dark:text-white md:text-lg text-right group-hover:text-orange-500">
+                        <h1 className="font-light text-xs text-black dark:text-white md:text-lg text-right group-hover:text-white">
                           {moment(data.createDate).utc().format('YYYY-MM-DD')}
                         </h1>
-                        <h1 className="font-light text-xs text-black dark:text-white md:text-lg text-right group-hover:text-orange-500">
+                        <h1 className="font-light text-xs text-black dark:text-white md:text-lg text-right group-hover:text-white">
                           {data.Location}
                         </h1>
                       </div>
@@ -145,7 +145,7 @@ export default function Jobs({categories, locations, latestjobs}) {
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-10">
                   {locations.map((data, index) => (
                     <button
-                      className="w-full flex items-center mb-3 group hover:bg-gray-200 px-2 lg:px-4 py-2"
+                      className="w-full flex items-center mb-3 group hover:bg-neutral-500 px-2 lg:px-4 py-2"
                       key={index}
                       type = "button"
                       onClick = {()=>{
@@ -157,10 +157,10 @@ export default function Jobs({categories, locations, latestjobs}) {
                     >
                       <Image src={data.Image == null ? "/images/bgImage1.avif" : data.Image} width={50} height={50} alt="image that will be displayed" />
                       <div className="flex flex-col ml-10">
-                        <h1 className="text-black dark:text-white font-normal text-xs md:text-lg lg:text-xl capitalize group-hover:text-orange-500 mb-5">
+                        <h1 className="text-black dark:text-white font-normal text-xs md:text-lg lg:text-xl capitalize group-hover:text-white mb-5">
                           jobs in {data.LocationName}
                         </h1>
-                        <h1 className="text-black dark:text-white text-left text-blue-800 font-bold text-xs md:text-lg lg:text-xl group-hover:text-orange-500 group-hover:border-orange-200">
+                        <h1 className="text-black dark:text-white text-left text-blue-800 font-bold text-xs md:text-lg lg:text-xl group-hover:text-white group-hover:border-orange-200">
                           {data._count.Job}
                         </h1>
                       </div>
@@ -173,7 +173,7 @@ export default function Jobs({categories, locations, latestjobs}) {
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 py-10">
                   {categories.map((data, index) => (
                     <button
-                      className="flex justify-between items-center mb-3 group hover:bg-gray-200 px-2 lg:px-4 py-2 w-full"
+                      className="flex justify-between items-center mb-3 group hover:bg-neutral-500 px-2 lg:px-4 py-2 w-full"
                       key={index}
                       type = "button"
                       onClick = {()=>{
@@ -183,10 +183,10 @@ export default function Jobs({categories, locations, latestjobs}) {
                         })
                       }}
                     >
-                      <h1 className="w-2/4 lg:w-3/4 text-left text-black dark:text-white font-normal text-xs md:text-lg lg:text-xl capitalize group-hover:text-orange-500">
+                      <h1 className="w-2/4 lg:w-3/4 text-left text-black dark:text-white font-normal text-xs md:text-lg lg:text-xl capitalize group-hover:text-white">
                         {data.CategoryName}
                       </h1>
-                      <h1 className="w-1/4 px-2 lg:px-5 py-2 text-black dark:text-white border rounded-xl border-gray-200 text-blue-800 font-bold text-xs md:text-lg lg:text-xl group-hover:text-orange-500 group-hover:border-orange-200">
+                      <h1 className="w-1/4 px-2 lg:px-5 py-2 text-black dark:text-white border rounded-xl border-gray-200 text-blue-800 font-bold text-xs md:text-lg lg:text-xl group-hover:text-white group-hover:border-orange-200">
                         {data._count.JobCategory}
                       </h1>
                     </button>
@@ -200,8 +200,8 @@ export default function Jobs({categories, locations, latestjobs}) {
               onClick={(e) => setJobs("latest")}
               className={
                 jobs == "latest"
-                  ? "py-3 bg-white dark:bg-slate-800 px-6 border border-slate-300 flex items-center hover:text-blue-400 border-x-white dark:border-none -ml-1"
-                  : "py-3 bg-gray-200 dark:bg-slate-600 px-6 border border-slate-300 dark:border-slate-700 flex items-center hover:bg-white hover:text-blue-400"
+                  ? "py-3 bg-neutral-400 dark:bg-slate-800 px-6 border border-slate-300 flex items-center hover:text-white dark:border-none -ml-1"
+                  : "py-3 bg-gray-200 dark:bg-slate-600 border-4 border-y-white dark:border-slate-700 flex items-center hover:bg-neutral-500 hover:text-white"
               }
             >
               <span className="font-bold text-md lg:text-lg">
@@ -216,8 +216,8 @@ export default function Jobs({categories, locations, latestjobs}) {
               onClick={(e) => setJobs("category")}
               className={
                 jobs == "category"
-                  ? "py-3 bg-white dark:bg-slate-800 px-6 border border-slate-300 flex items-center hover:text-blue-400 border-x-white dark:border-none -ml-1"
-                  : "py-3 bg-gray-200 dark:bg-slate-600 px-6 border border-slate-300 dark:border-slate-700 flex items-center hover:bg-white hover:text-blue-400"
+                  ? "py-3 bg-neutral-400 dark:bg-slate-800 px-6 border border-slate-300 flex items-center hover:text-white dark:border-none -ml-1"
+                  : "py-3 bg-gray-200 dark:bg-slate-600 border-4 border-y-white dark:border-slate-700 flex items-center hover:bg-neutral-500 hover:text-white"
               }
             >
               <span className="font-bold text-md lg:text-lg">
@@ -231,8 +231,8 @@ export default function Jobs({categories, locations, latestjobs}) {
               onClick={(e) => setJobs("location")}
               className={
                 jobs == "location"
-                  ? "py-3 bg-white dark:bg-slate-800 px-6 border border-slate-300 flex items-center hover:text-blue-400 border-x-white dark:border-none -ml-1"
-                  : "py-3 bg-gray-200 dark:bg-slate-600 px-6 border border-slate-300 dark:border-slate-700 flex items-center hover:bg-white hover:text-blue-400"
+                  ? "py-3 bg-neutral-400 dark:bg-slate-800 px-6 border border-slate-300 flex items-center hover:text-white dark:border-none -ml-1"
+                  : "py-3 bg-gray-200 dark:bg-slate-600 border-4 border-y-white dark:border-slate-700 flex items-center hover:bg-neutral-500 hover:text-white"
               }
             >
               <span className="font-bold text-md lg:text-lg">
