6195762c4eec6b127a2b26f15fcadf60e0b5cbbb
yafet-123 <yafetaddisu123@gmail.com>
2023-02-14 13:22:07 -0800

    fixing the error

--- pages/Courses.js
+++ pages/Courses.js
@@ -9,8 +9,8 @@ export default function Courses() {
   return (
   	<React.Fragment>
       <MainHeader title="Courses" />
-	    <section className="flex flex-col w-full h-full px-0 md:px-32  bg-gray-300 dark:bg-slate-700">
-	    	<div className="grid grid-cols-1 lg:grid-cols-2 gap-10 bg-white dark:bg-slate-800 pt-20">
+	    <section className="flex flex-col w-full h-full px-0 md:px-32 py-32 bg-neutral-300 dark:bg-slate-700">
+	    	<div className="grid grid-cols-1 lg:grid-cols-2 gap-10 bg-neutral-100 dark:bg-slate-800 pt-20">
 	    		{ CourseHead.map((data,index)=>(
 		    		<div 
 		    			className={`flex flex-col justify-center items-center mb-10 mx-2 lg:m-20 p-10 rounded-xl ${data.backgroundColor}`} 
