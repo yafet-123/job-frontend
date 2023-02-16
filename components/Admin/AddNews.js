03e6458e79dac023ff43e21e54dbd78a83453947
yafet-123 <yafetaddisu123@gmail.com>
2023-02-16 14:08:09 -0800

    editing the admin page

--- components/Admin/AddNews.js
+++ components/Admin/AddNews.js
@@ -26,7 +26,7 @@ const QuillNoSSRWrapper = dynamic(
 )
 
 
-export default function AddNews({categories}) {
+export function AddNews({categories}) {
     const router = useRouter();
     
     const [Header, setHeader] = useState("")
@@ -158,7 +158,7 @@ export default function AddNews({categories}) {
                         />
                         <label 
                             htmlFor="floating_outlined" 
-                            className="absolute text-md lg:text-xl text-black dark:text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-gray-100 dark:bg-slate-700 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+                            className="absolute text-md lg:text-xl text-black dark:text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-neutral-300 dark:bg-slate-700 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
                         >
                             Header
                         </label>
