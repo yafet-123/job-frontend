d03d47528092f50d3ec98867a3371b6871cf46be
yafet-123 <yafetaddisu123@gmail.com>
2023-02-25 06:18:06 -0800

    editing the admin page

--- pages/Jobs.js
+++ pages/Jobs.js
@@ -11,6 +11,7 @@ import { useRouter } from 'next/router'
 import { prisma } from '../util/db.server.js'
 import moment from 'moment';
 import { MainHeader } from '../components/MainHeader';
+import { Hero } from "../components/Hero";
 export async function getServerSideProps(){
   const categories = await prisma.Category.findMany({
     include:{
@@ -84,162 +85,165 @@ export default function Jobs({categories, locations, latestjobs}) {
   return (
     <React.Fragment>
       <MainHeader title="Jobs" />
-      <section className="flex flex-col w-full h-full py-20 px-0 md:px-24 bg-[#ddd0c8] dark:bg-slate-700 pt-32">
-        <div className="flex flex-col lg:flex-row justify-between items-center h-16 w-full mb-10 px-10">
-          <h1 className="text-black dark:text-white font-light text-md md:text-3xl lg:text-4xl capitalize lg:mb-5 text-center">
-            Search and Find Jobs in Ethiopia
-          </h1>
+      <section className="flex flex-col w-full h-full py-20 pt-32 bg-[#ddd0c8] dark:bg-slate-700">
+        <Hero />
+        <div className="px-0 md:px-24 bg-[#d1cbc7] dark:bg-slate-800 pt-16">
+          <div className="flex flex-col lg:flex-row justify-between items-center h-16 w-full mb-10 px-10">
+            <h1 className="text-black dark:text-white font-light text-md md:text-3xl lg:text-4xl capitalize lg:mb-5 text-center">
+              Search and Find Jobs in Ethiopia
+            </h1>
 
-          <button 
-            className={`text-black dark:text-white text-black text-md md:text-2xl lg:text-3xl focus:bg-red-400 p-4 rounded-xl`} 
-            onClick={() => router.push("/AdvanceSearch")}
-          >
-            Advance Search
-          </button>
-        </div>
-    
-        <hr className="w-full bg-gray-200 mb-5" />
-
-        <div className="flex w-full h-[25rem] lg:h-[45rem] bg-neutral-100 dark:bg-slate-800 border rounded-xl p-2">
-          <div className="flex-1 border rounded-xl shadow-2xl shadow-sky-200 border-slate-300 dark:border-slate-800 lg:p-5">
-            <div className="w-full h-full overflow-y-scroll">
-              {jobs == "latest" && (
-                <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-5 py-10">
-                  {latestjobs.map((data, index) => (
-                    <button
-                      className="flex justify-between items-center mb-3 group hover:bg-neutral-500 px-2 lg:px-4 py-2 w-full"
-                      key={index}
-                      type = "button"
-                      onClick = {()=>{
-                        router.push({
-                          pathname:"/DisplayJobs",
-                          query:{job_id:data.job_id}
-                        })
-                      }}
-                    >
-                      <div className="flex flex-col w-2/4 lg:w-3/4">
-                        <h1 className="font-normal text-sm lg:text-lg text-blue-500 dark:text-white group-hover:text-white text-left">
-                          {data.JobsType}
-                        </h1>
-                        <h1 className="font-light text-xs lg:text-sm text-black dark:text-white group-hover:text-white text-left">
-                          {data.CompanyName}
-                        </h1>
-                      </div>
-                      <div className="flex flex-col items-center justify-center w-1/4 lg:w-1/4">
-                        <h1 className="font-light text-xs text-black dark:text-white md:text-lg text-right group-hover:text-white">
-                          {moment(data.createDate).utc().format('YYYY-MM-DD')}
-                        </h1>
-                        <h1 className="font-light text-xs text-black dark:text-white md:text-lg text-right group-hover:text-white">
-                          {data.Location}
-                        </h1>
-                      </div>
-                    </button>
-                  ))}
-                </div>
-              )}
-
-              {jobs == "location" && (
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-10">
-                  {locations.map((data, index) => (
-                    <button
-                      className="w-full flex items-center mb-3 group hover:bg-neutral-500 px-2 lg:px-4 py-2"
-                      key={index}
-                      type = "button"
-                      onClick = {()=>{
-                        router.push({
-                          pathname:"/JobsByLocation",
-                          query:{location:data.LocationName, howmany:data._count.Job, image:data.Image, location_id:data.location_id}
-                        })
-                      }}
-                    >
-                      <Image src={data.Image == null ? "/images/bgImage1.avif" : data.Image} width={50} height={50} alt="image that will be displayed" />
-                      <div className="flex flex-col ml-10">
-                        <h1 className="text-black dark:text-white font-normal text-xs md:text-lg lg:text-xl capitalize group-hover:text-white mb-5">
-                          jobs in {data.LocationName}
-                        </h1>
-                        <h1 className="text-black dark:text-white text-left text-blue-800 font-bold text-xs md:text-lg lg:text-xl group-hover:text-white group-hover:border-orange-200">
-                          {data._count.Job}
-                        </h1>
-                      </div>
-                    </button>
-                  ))}
-                </div>
-              )}
-
-              {jobs == "category" && (
-                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 py-10">
-                  {categories.map((data, index) => (
-                    <button
-                      className="flex justify-between items-center mb-3 group hover:bg-neutral-500 px-2 lg:px-4 py-2 w-full"
-                      key={index}
-                      type = "button"
-                      onClick = {()=>{
-                        router.push({
-                          pathname:"/JobsByCategory",
-                          query:{category: data.CategoryName, howmany:data._count.JobCategory, category_id: data.category_id}
-                        })
-                      }}
-                    >
-                      <h1 className="w-2/4 lg:w-3/4 text-left text-black dark:text-white font-normal text-xs md:text-lg lg:text-xl capitalize group-hover:text-white">
-                        {data.CategoryName}
-                      </h1>
-                      <h1 className="w-1/4 px-2 lg:px-5 py-2 text-black dark:text-white border rounded-xl border-gray-200 text-blue-800 font-bold text-xs md:text-lg lg:text-xl group-hover:text-white group-hover:border-orange-200">
-                        {data._count.JobCategory}
-                      </h1>
-                    </button>
-                  ))}
-                </div>
-              )}
-            </div>
+            <button 
+              className={`text-black dark:text-white text-black text-md md:text-2xl lg:text-3xl focus:bg-red-400 p-4 rounded-xl`} 
+              onClick={() => router.push("/AdvanceSearch")}
+            >
+              Advance Search
+            </button>
           </div>
-          <ul className="mt-5">
-            <li
-              onClick={(e) => setJobs("latest")}
-              className={
-                jobs == "latest"
-                  ? "py-3 bg-neutral-400 dark:bg-slate-800 px-6 border border-slate-300 flex items-center hover:text-white dark:border-none -ml-1"
-                  : "py-3 bg-gray-200 dark:bg-slate-600 border-4 border-y-white dark:border-slate-700 flex items-center hover:bg-neutral-500 hover:text-white"
-              }
-            >
-              <span className="font-bold text-md lg:text-lg">
-                <FaClock />
-              </span>
-              <span className="hidden lg:inline-flex text-xl ml-3 capitalize">
-                View latest jobs
-              </span>
-            </li>
+      
+          <hr className="w-full bg-gray-200 mb-5" />
 
-            <li
-              onClick={(e) => setJobs("category")}
-              className={
-                jobs == "category"
-                  ? "py-3 bg-neutral-400 dark:bg-slate-800 px-6 border border-slate-300 flex items-center hover:text-white dark:border-none -ml-1"
-                  : "py-3 bg-gray-200 dark:bg-slate-600 border-4 border-y-white dark:border-slate-700 flex items-center hover:bg-neutral-500 hover:text-white"
-              }
-            >
-              <span className="font-bold text-md lg:text-lg">
-                <BiCategory />
-              </span>
-              <span className="hidden lg:inline-flex text-xl ml-3">
-                Jobs by Category
-              </span>
-            </li>
-            <li
-              onClick={(e) => setJobs("location")}
-              className={
-                jobs == "location"
-                  ? "py-3 bg-neutral-400 dark:bg-slate-800 px-6 border border-slate-300 flex items-center hover:text-white dark:border-none -ml-1"
-                  : "py-3 bg-gray-200 dark:bg-slate-600 border-4 border-y-white dark:border-slate-700 flex items-center hover:bg-neutral-500 hover:text-white"
-              }
-            >
-              <span className="font-bold text-md lg:text-lg">
-                <GoLocation />
-              </span>
-              <span className="hidden lg:inline-flex text-xl ml-3">
-                Jobs by Location
-              </span>
-            </li>
-          </ul>
+          <div className="flex w-full h-[25rem] lg:h-[45rem] bg-neutral-100 dark:bg-slate-800 border rounded-xl p-2">
+            <div className="flex-1 border rounded-xl shadow-2xl shadow-sky-200 border-slate-300 dark:border-slate-800 lg:p-5">
+              <div className="w-full h-full overflow-y-scroll">
+                {jobs == "latest" && (
+                  <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-5 py-10">
+                    {latestjobs.map((data, index) => (
+                      <button
+                        className="flex justify-between items-center mb-3 group hover:bg-neutral-500 px-2 lg:px-4 py-2 w-full"
+                        key={index}
+                        type = "button"
+                        onClick = {()=>{
+                          router.push({
+                            pathname:"/DisplayJobs",
+                            query:{job_id:data.job_id}
+                          })
+                        }}
+                      >
+                        <div className="flex flex-col w-2/4 lg:w-3/4">
+                          <h1 className="font-normal text-sm lg:text-lg text-blue-500 dark:text-white group-hover:text-white text-left">
+                            {data.JobsType}
+                          </h1>
+                          <h1 className="font-light text-xs lg:text-sm text-black dark:text-white group-hover:text-white text-left">
+                            {data.CompanyName}
+                          </h1>
+                        </div>
+                        <div className="flex flex-col items-center justify-center w-1/4 lg:w-1/4">
+                          <h1 className="font-light text-xs text-black dark:text-white md:text-lg text-right group-hover:text-white">
+                            {moment(data.createDate).utc().format('YYYY-MM-DD')}
+                          </h1>
+                          <h1 className="font-light text-xs text-black dark:text-white md:text-lg text-right group-hover:text-white">
+                            {data.Location}
+                          </h1>
+                        </div>
+                      </button>
+                    ))}
+                  </div>
+                )}
+
+                {jobs == "location" && (
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-10">
+                    {locations.map((data, index) => (
+                      <button
+                        className="w-full flex items-center mb-3 group hover:bg-neutral-500 px-2 lg:px-4 py-2"
+                        key={index}
+                        type = "button"
+                        onClick = {()=>{
+                          router.push({
+                            pathname:"/JobsByLocation",
+                            query:{location:data.LocationName, howmany:data._count.Job, image:data.Image, location_id:data.location_id}
+                          })
+                        }}
+                      >
+                        <Image src={data.Image == null ? "/images/bgImage1.avif" : data.Image} width={50} height={50} alt="image that will be displayed" />
+                        <div className="flex flex-col ml-10">
+                          <h1 className="text-black dark:text-white font-normal text-xs md:text-lg lg:text-xl capitalize group-hover:text-white mb-5">
+                            jobs in {data.LocationName}
+                          </h1>
+                          <h1 className="text-black dark:text-white text-left text-blue-800 font-bold text-xs md:text-lg lg:text-xl group-hover:text-white group-hover:border-orange-200">
+                            {data._count.Job}
+                          </h1>
+                        </div>
+                      </button>
+                    ))}
+                  </div>
+                )}
+
+                {jobs == "category" && (
+                  <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 py-10">
+                    {categories.map((data, index) => (
+                      <button
+                        className="flex justify-between items-center mb-3 group hover:bg-neutral-500 px-2 lg:px-4 py-2 w-full"
+                        key={index}
+                        type = "button"
+                        onClick = {()=>{
+                          router.push({
+                            pathname:"/JobsByCategory",
+                            query:{category: data.CategoryName, howmany:data._count.JobCategory, category_id: data.category_id}
+                          })
+                        }}
+                      >
+                        <h1 className="w-2/4 lg:w-3/4 text-left text-black dark:text-white font-normal text-xs md:text-lg lg:text-xl capitalize group-hover:text-white">
+                          {data.CategoryName}
+                        </h1>
+                        <h1 className="w-1/4 px-2 lg:px-5 py-2 text-black dark:text-white border rounded-xl border-gray-200 text-blue-800 font-bold text-xs md:text-lg lg:text-xl group-hover:text-white group-hover:border-orange-200">
+                          {data._count.JobCategory}
+                        </h1>
+                      </button>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </div>
+            <ul className="mt-5">
+              <li
+                onClick={(e) => setJobs("latest")}
+                className={
+                  jobs == "latest"
+                    ? "py-3 bg-neutral-400 dark:bg-slate-800 px-6 border border-slate-300 flex items-center hover:text-white dark:border-none -ml-1"
+                    : "py-3 bg-gray-200 dark:bg-slate-600 border-4 border-y-white dark:border-slate-700 flex items-center hover:bg-neutral-500 hover:text-white"
+                }
+              >
+                <span className="font-bold text-md lg:text-lg">
+                  <FaClock />
+                </span>
+                <span className="hidden lg:inline-flex text-xl ml-3 capitalize">
+                  View latest jobs
+                </span>
+              </li>
+
+              <li
+                onClick={(e) => setJobs("category")}
+                className={
+                  jobs == "category"
+                    ? "py-3 bg-neutral-400 dark:bg-slate-800 px-6 border border-slate-300 flex items-center hover:text-white dark:border-none -ml-1"
+                    : "py-3 bg-gray-200 dark:bg-slate-600 border-4 border-y-white dark:border-slate-700 flex items-center hover:bg-neutral-500 hover:text-white"
+                }
+              >
+                <span className="font-bold text-md lg:text-lg">
+                  <BiCategory />
+                </span>
+                <span className="hidden lg:inline-flex text-xl ml-3">
+                  Jobs by Category
+                </span>
+              </li>
+              <li
+                onClick={(e) => setJobs("location")}
+                className={
+                  jobs == "location"
+                    ? "py-3 bg-neutral-400 dark:bg-slate-800 px-6 border border-slate-300 flex items-center hover:text-white dark:border-none -ml-1"
+                    : "py-3 bg-gray-200 dark:bg-slate-600 border-4 border-y-white dark:border-slate-700 flex items-center hover:bg-neutral-500 hover:text-white"
+                }
+              >
+                <span className="font-bold text-md lg:text-lg">
+                  <GoLocation />
+                </span>
+                <span className="hidden lg:inline-flex text-xl ml-3">
+                  Jobs by Location
+                </span>
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
     </React.Fragment>
