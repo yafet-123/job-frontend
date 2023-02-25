66912f213bdda2cdf73aa9c68930c5f223abba71
yafet-123 <yafetaddisu123@gmail.com>
2023-02-25 07:56:22 -0800

    editing the admin page

--- pages/Admin/Category.js
+++ pages/Admin/Category.js
@@ -40,7 +40,7 @@ export default function Category({categorie}) {
     return (
     	<React.Fragment>
       	<MainHeader title="Category Dashboard" />
-        	<section className="flex flex-col w-full h-full bg-gray-300 dark:bg-slate-700 pt-28">
+        	<section className="flex flex-col w-full h-full bg-[#d1cbc7] dark:bg-slate-700 pt-28">
     				<div className='w-full h-full flex flex-col lg:flex-row'>
     		      <VerticalNavbar data={data} />
     		      <div className="w-full">
