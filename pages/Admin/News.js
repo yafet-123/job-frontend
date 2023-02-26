5f09bf823951859bf224949950cb71b9a64eb171
yafet-123 <yafetaddisu123@gmail.com>
2023-02-26 08:58:12 -0800

    fixing the error

--- pages/Admin/News.js
+++ pages/Admin/News.js
@@ -5,16 +5,46 @@ import { useSession } from "next-auth/react";
 import { VerticalNavbar } from "../../components/Admin/VerticalNavbar";
 import { MainHeader } from '../../components/MainHeader';
 
-export default function News() {
+export async function getServerSideProps(){
+  const newscategories = await prisma.NewsCategory.findMany({
+    orderBy: {
+      category_id:"asc"
+    },
+    include:{
+      User:{
+          select:{
+              UserName:true
+          }
+      }
+    }
+  })
+
+  const AllNewscategories = newscategories.map((data)=>({
+      category_id:data.category_id,
+      CategoryName:data.CategoryName,
+      CreatedDate:data.CreatedDate,
+      ModifiedDate:data.ModifiedDate,
+      userName:data.User.UserName
+  }))
+
+  return{
+    props:{
+      categories:JSON.parse(JSON.stringify(AllNewscategories)),
+    }
+  }
+}
+
+export default function News({categories}) {
     const { status, data } = useSession();
     return (
-    	<React.Fragment>
+    	
+        <React.Fragment>
           	<MainHeader title="Category Dashboard" />
         	<section className="flex flex-col w-full h-full bg-[#ddd0c8] dark:bg-slate-700 pt-10">
     		    <div className='w-full h-full flex flex-col lg:flex-row'>
     		        <VerticalNavbar data={data} />
     		        <div className="w-full">
-            		    
+            		    <AddNews categories={categories} />
             	    </div>
     		    </div> 
   			</section>
