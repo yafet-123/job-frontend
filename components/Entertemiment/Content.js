52996ca5176eb03a7ecdd548b8aaaa97e5fa4e19
yafet-123 <yafetaddisu123@gmail.com>
2023-02-26 09:08:27 -0800

    fixing the error

--- components/Entertemiment/Content.js
+++ components/Entertemiment/Content.js
@@ -15,13 +15,13 @@ export function Content({entertainments}) {
                              <input 
                                  id="search" 
                                  type="text" 
-                                 className="block w-full px-2 lg:px-3 text-md lg:text-xl text-black dark:text-white bg-transparent py-4 border-2 border-black rounded-xl appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-500 peer" placeholder=" "
+                                 className="block w-full px-2 lg:px-3 text-md lg:text-xl text-black dark:text-white bg-[#ddd0c8] py-4 border-2 border-black rounded-xl appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-500 peer" placeholder=" "
                                  value={getSearchValue}
                                  onChange={(e) => setgetSearchValue(e.target.value)}
                              />
                              <label 
                                  htmlFor="floating_outlined" 
-                                 className="absolute text-md lg:text-xl text-black dark:text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-neutral-300 dark:bg-slate-700 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
+                                 className="absolute text-md lg:text-xl text-black dark:text-white duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-[#ddd0c8] dark:bg-slate-700 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1"
                              >
                                  Search
                              </label>
