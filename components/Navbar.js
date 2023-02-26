b8f38303fe4a3f1faba8cc3963dd50b8db7efcc7
yafet-123 <yafetaddisu123@gmail.com>
2023-02-26 10:14:52 -0800

    fixing the error

--- components/Navbar.js
+++ components/Navbar.js
@@ -43,7 +43,7 @@ export function Navbar() {
   return (
     <nav
       className={ router.pathname == "/auth/signin" || router.pathname == '/Forgotpassword' ? "hidden" : `${
-        colorChange ? 'bg-white bg-opacity-80 dark:bg-slate-800 ' : 'bg-transparent'
+        colorChange ? 'bg-white bg-opacity-80 dark:bg-slate-800 ' : `${open ? 'bg-white' : 'bg-transparent'}`
       } w-full   lg:h-[110px] top-0 fixed z-50`}
     >
       <div className="lg:justify-between justify-around lg:px-4 mx-10 items-center lg:flex lg:py-[10px] ">
