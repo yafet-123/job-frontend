7904467dfff337c18a5586affbc0f56386552d00
yafet-123 <yafetaddisu123@gmail.com>
2023-03-04 14:50:29 -0800

    adding css,js and python page

--- pages/Courses.js
+++ pages/Courses.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { MainHeader } from '../components/MainHeader';
-import { CourseHeadData } from '../data/CourseHead'
+import { CourseHeadData } from '../data/courseHead'
 
 export default function Courses() {
 	const router = useRouter()
