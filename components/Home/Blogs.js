d3641dcc840ecf2f8f8b71d2728063b488e40781
yafet-123 <yafetaddisu123@gmail.com>
2023-04-05 10:30:12 -0700

    editing the home page

--- components/Home/Blogs.js
+++ components/Home/Blogs.js
@@ -34,7 +34,7 @@ export function Blogs({blogs}) {
   }
 
   return (
-    <section className="w-full h-full lg:h-[50rem] md:px-10 md:py-10 bg-slate-100 dark:bg-slate-700 flex flex-col brightness-100 py-10">
+    <section className="w-full h-full lg:h-[50rem] md:py-10 bg-slate-100 dark:bg-slate-700 flex flex-col brightness-100 py-10">
       <h1 className={`px-3 lg:px-0 text-lg mb-3 lg:mb-10 font-bold md:text-2xl text-center lg:text-5xl text-black dark:text-white opacity-100`}>
         Blogs
       </h1>
