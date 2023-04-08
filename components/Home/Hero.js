f5f9f4eeea1486caebdd35bb5c7a62108ce57eaa
yafet-123 <yafetaddisu123@gmail.com>
2023-04-08 03:57:53 -0700

    editing the search page

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -7,7 +7,13 @@ export function Hero() {
   const [search,setsearch] = useState("job")
   const router = useRouter();
   const [searchValue, setsearchValue] = useState("")
-
+  async function registerCategory(e){
+        e.preventDefault()
+        router.push({
+          pathname: '/AdvanceSearch',
+          query: { searchName: searchValue },
+        })
+    }
   return (
     <div className="w-full h-[20rem] lg:h-[25rem] bg-[#e6e6e6] bg-opacity-100 dark:bg-[#02201D] relative">
       <div className="absolute top-32 lg:top-44 flex flex-col justify-between left-0 right-0 m-auto w-full lg:w-[70%] px-2 lg:text-lg">
@@ -15,7 +21,7 @@ export function Hero() {
           Hulu Media Hulu Neger
         </h1>
 
-        <div className="!h-16 w-full dark:border-slate-800 px-2">
+        <form className="!h-16 w-full dark:border-slate-800 px-2" onSubmit={handleSearch}>
           <div className="flex flex-col lg:flex-row justify-center items-center ">
             <input 
               value={searchValue} 
@@ -28,17 +34,13 @@ export function Hero() {
             <div className="h-16 bg-[#009688] text-white lg:px-3 flex items-center justify-center border border[#009688] border-l-2 rounded-xl px-5">
               <AiOutlineSearch size={20} />
               <button 
-                onClick={()=> router.push({
-                  pathname: '/AdvanceSearch',
-                  query: { searchName: searchValue },
-                })} 
                 className="font-bold text-xs md:text-xl text-white bg-[#009688] lg:px-3 flex items-center justify-center "
               >
                 Search
               </button>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
