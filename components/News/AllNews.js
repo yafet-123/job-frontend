f2fafb63668d9ec5e1e80b2c62c309b15629793d
yafet-123 <yafetaddisu123@gmail.com>
2023-04-11 13:43:20 -0700

    editing the sharing button of the page

--- components/News/AllNews.js
+++ components/News/AllNews.js
@@ -67,7 +67,7 @@ export function AllNews({allnews}) {
                   onClick={() => {
                       clickedForview()
                       setid(news_id)
-                      setquotes(quoteRef.current?.innerHTML)
+                      setquotes(quoteRef.current?.textContent)
                   }} 
                   className="text-black dark:text-white hover:text-[#009688] font-bold py-2 px-4 hover:scale-110 duration-1000 ease-in-out rounded ">
                   <AiOutlineShareAlt size={32} />
--- components/common/Share.js
+++ components/common/Share.js
@@ -65,7 +65,7 @@ export function Share({shareUrl,id,setviewModalOn,quote}) {
               <div className="m-1 lg:m-3 group hover:bg-[#009688] p-2 hover:border rounded-2xl">
                 <TelegramShareButton
                   url={`https://job-frontend-main.vercel.app${shareUrl}#${id}`}
-                  title={quote}
+                  title={<div dangerouslySetInnerHTML={{ __html: quote }} />}
                   hashtag={'#huluMedia'}
                   className="flex flex-col items-center justify-between"
                 >
