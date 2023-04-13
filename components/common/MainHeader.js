ed38ff999efa5e03574b9227ac5ba7e7c280a268
yafet-123 <yafetaddisu123@gmail.com>
2023-04-13 11:09:26 -0700

    editing the og image

--- components/common/MainHeader.js
+++ components/common/MainHeader.js
@@ -3,18 +3,18 @@ import Head from 'next/head';
 
 export const MainHeader = ({ title }) => {
   const image = "https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg"
-  const url = `http://localhost:3000/api/og?images=${image}`
+  const url = `${process.env.headurl}?images=${image}`
   return (
     <div>
       <Head>
         <title>{title}</title>
-        <meta
-          name="description"
-          content="Hulu Media Offer you diffrent jobs , entertainment and other wonderfull things"
-        />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta property = "og:image" content ={url} />
+        <meta property="og:url" content={process.env.headeurl} key="ogurl" />
+        <meta property="og:image" content={url} key="ogimage" />
+        <meta property="og:site_name" content="Hulu Media" key="ogsitename" />
+        <meta property="og:title" content={title} key="ogtitle" />
+        <meta property="og:description" content="Hulu Media Offer you diffrent jobs , entertainment and other wonderfull things" key="ogdesc" />
         <link
           rel="Head-icon"
           sizes="180x180"
