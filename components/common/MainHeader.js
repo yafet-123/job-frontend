506ba597c088efd67873247dcb08ddb9f027fcb2
yafet-123 <yafetaddisu123@gmail.com>
2023-04-17 09:34:48 -0700

    editing the news and et display all content page

--- components/common/MainHeader.js
+++ components/common/MainHeader.js
@@ -3,20 +3,18 @@ import Head from 'next/head';
 
 export const MainHeader = ({ title }) => {
   const image = "https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg"
-  const url = `${process.env.headurl}?images=${image}`
+  const url = `${process.env.headurl}/api/og?images=${image}`
   return (
     <div>
       <Head>
         <title>{title}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          property="og:description"
-          content="And a social description for our cool page"
-        />
-        <meta
-          property="og:image"
-          content="https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg"
-        />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta property="og:url" content={process.env.headeurl} key="ogurl" />
+        <meta property="og:image" content={url} key="ogimage" />
+        <meta property="og:site_name" content="Hulu Media" key="ogsitename" />
+        <meta property="og:title" content={title} key="ogtitle" />
+        <meta property="og:description" content="Hulu Media Offer you diffrent jobs , entertainment and other wonderfull things" key="ogdesc" />
         
       </Head>
     </div>
