c159dd965e9b8fd22bb5bd3ba2368fec73c4f7b7
yafet-123 <yafetaddisu123@gmail.com>
2023-04-21 06:23:19 -0700

    editing the allnews page api

--- pages/api/og.jsx
+++ pages/api/og.jsx
@@ -5,9 +5,27 @@ export const config = {
 };
  
 export default function (req) {
-  const { searchParams } = new URL(req.url)
+  const { searchParams } = req.nextUrl;
   const image = searchParams.get('images')
-  console.log("image")
+  console.log(image)
+
+  if(!image) {
+    return new ImageResponse(
+      <img
+        alt="background image that will be shared"
+        src="https://cdn.pixabay.com/photo/2023/04/06/01/26/heart-7902540_960_720.jpg"
+        style={{
+          width:"100%",
+          height:"100%",
+          backgroundPosition:"center",
+          backgroundSize: "cover",
+        }}
+      />
+      ,{
+      width:1200,
+      height:630,
+    });
+  }
   return new ImageResponse(
     (
       <div
