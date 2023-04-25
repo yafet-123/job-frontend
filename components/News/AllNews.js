d30c10d2bdaf706d41619b8365b8d4e95c313c3f
yafet-123 <yafetaddisu123@gmail.com>
2023-04-25 12:47:35 -0700

    editing the news page all

--- components/News/AllNews.js
+++ components/News/AllNews.js
@@ -4,7 +4,8 @@ import moment from 'moment';
 import { useRouter } from 'next/router'
 import {Share} from '../common/Share.js'
 import { AiOutlineShareAlt, AiOutlineEye } from 'react-icons/ai'
- 
+import Link from 'next/link'
+
 export function AllNews({allnews , setimage, setquotes}) {
   const router = useRouter()
   const quoteRef = useRef(null)
@@ -17,37 +18,36 @@ export function AllNews({allnews , setimage, setquotes}) {
 
   return (
     <div className="py-5 w-full h-full">      
-      <ul className="allListGrid w-full">
+      <div className="allListGrid w-full">
         {allnews.map(({news_id, CreatedDate, Header, ShortDescription, image, Category, view},index)=>(
-          <li className="group" key={index} 
-            onClick = {()=>{
-              router.push({
-                pathname:"/DisplayNews",
-                query:{news_id:news_id}
-              })
-            }}
+          <Link key={index} 
+            href={`/DisplayNews?news_id=${news_id}`}
           >
-            <div className="relative">
-              <Image src={image} fill className="!bg-cover w-full !h-full border rounded-xl" alt="latest news image"/>
-            </div>
-            <div className="flex flex-col justify-between mt-5">
-              <h1 className="allListHeader group-hover:text-[#009688] text-left group-hover:underline font-extrabold dark:text-[#009688] text-slate-600 tracking-wide leading-snug">
-                {Header}
-              </h1>
-              <div  className="allListshortdescription w-full group-hover:text-xl group-hover:text-[#009688] bg-transparent text-black dark:!text-white" dangerouslySetInnerHTML={{ __html: ShortDescription }} />
-              <div className="w-full flex flex-row justify-between items-center px-2 mt-5">
-                <h3 className="text-left font-normal text-sm lg:text-md dark:text-white text-slate-600">
-                  {moment(CreatedDate).utc().format('MMMM, Do YYYY')}
-                </h3>
-                <p className="flex flex-row items-center text-black dark:text-white hover:text-[#009688] font-bold py-2 hover:scale-110 duration-1000 ease-in-out rounded ">
-                  <AiOutlineEye size={20} />
-                  <span className="ml-3 text-xs">{view}</span>
-                </p>
+            <a className="group">
+              <div className="relative">
+                <Image src={image} fill className="!bg-cover w-full !h-full border rounded-xl" alt="latest news image"/>
               </div>
-            </div>
-          </li>
+              <div className="flex flex-col justify-between mt-5">
+                <h1 className="allListHeader group-hover:text-[#009688] text-left group-hover:underline font-extrabold dark:text-[#009688] text-slate-600 tracking-wide leading-snug">
+                  {Header}
+                </h1>
+                <div  className="allListshortdescription w-full group-hover:text-xl group-hover:text-[#009688] bg-transparent text-black dark:!text-white" dangerouslySetInnerHTML={{ __html: ShortDescription }} />
+                <div className="w-full flex flex-row justify-between items-center px-2 mt-5">
+                  <h3 className="text-left font-normal text-sm lg:text-md dark:text-white text-slate-600">
+                    {moment(CreatedDate).utc().format('MMMM, Do YYYY')}
+                  </h3>
+
+
+                  <p className="flex flex-row items-center text-black dark:text-white hover:text-[#009688] font-bold py-2 hover:scale-110 duration-1000 ease-in-out rounded ">
+                    <AiOutlineEye size={20} />
+                    <span className="ml-3 text-xs">{view}</span>
+                  </p>
+                </div>
+              </div>
+            </a>
+          </Link>
         ))}
-      </ul>
+      </div>
 
       {viewmodalOn && 
         <Share setviewModalOn={setviewModalOn} shareUrl={shareUrl} id={id}/>
