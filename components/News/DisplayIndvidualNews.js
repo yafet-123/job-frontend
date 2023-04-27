6f96d3eb18ce2c45af5f218d146581b1afac4ebf
yafet-123 <yafetaddisu123@gmail.com>
2023-04-27 14:56:56 -0700

    editing the related topics style

--- components/News/DisplayIndvidualNews.js
+++ components/News/DisplayIndvidualNews.js
@@ -65,32 +65,25 @@ export function DisplayIndvidualNews({news, newsCategory,AllcategoryNews,shareUr
 	              				}}
 	              				id={data.News.news_id} ref={quoteRef} className="flex flex-col w-full lg:mt-5 group pt-5"
 	            			>
-	            				<div className="w-full !h-52 lg:!h-96 relative">
+	            				<div className="w-full !h-52 lg:!h-72 relative">
 	                				<Image src={data.News.Image} fill className="!bg-cover w-full !h-full border rounded-xl" alt="latest news image"/>
 	              				</div>
 
-	              				<h1 className="text-left group-hover:text-3xl group-hover:text-[#009688] group-hover:underline text-lg lg:text-2xl font-extrabold dark:text-[#009688] text-slate-600 tracking-wide leading-snug">
+	              				<h1 className="text-left group-hover:text-[#009688] group-hover:underline text-lg lg:text-xl font-extrabold dark:text-[#009688] text-slate-600 tracking-wide leading-snug">
 	                  				{data.News.Header}
 	                			</h1>
-	                			<div  className="group-hover:text-xl group-hover:text-[#009688] bg-transparent text-black dark:!text-white mt-5 ql-editor ql-snow ql-video " dangerouslySetInnerHTML={{ __html: data.News.ShortDescription }} />
+	                			<div  className="text-lg group-hover:text-[#009688] bg-transparent text-black dark:!text-white text-left mt-2" dangerouslySetInnerHTML={{ __html: data.News.ShortDescription }} />
 	            			</button>
 
 	            			<div className="flex items-center justify-between text-sm"> 
+	            				<h3 className="text-left font-normal text-sm lg:text-md dark:text-white text-slate-600">
+                                	{moment(data.CreatedDate).utc().format('MMMM, Do YYYY')}
+                                </h3>
+
 	              				<p className="flex flex-row items-center text-black dark:text-white hover:text-[#009688] font-bold py-2 hover:scale-110 duration-1000 ease-in-out rounded ">
 	                				<AiOutlineEye size={32} />
 	                				<span className="ml-3">{data.News.view}</span>
 	              				</p>
-
-	              				<button
-	                  				onClick={() => {
-	                      				clickedForview()
-	                      				setid(data.News.news_id)
-	                      				setquotes(quote)
-	                      				setimage(image)
-	                  				}} 
-	                  				className="text-black dark:text-white hover:text-[#009688] font-bold py-2 px-4 hover:scale-110 duration-1000 ease-in-out rounded ">
-	                  				<AiOutlineShareAlt size={32} />
-	              				</button>
 	            			</div>
 	            		</div>
 		        	))}
