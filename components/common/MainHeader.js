fb8304523f68370a13d037a4ca6069502649bc5a
yafet-123 <yafetaddisu123@gmail.com>
2023-05-05 09:33:00 -0700

    editing the main header og tags for theurl

--- components/common/MainHeader.js
+++ components/common/MainHeader.js
@@ -14,7 +14,7 @@ export const MainHeader = ({ title, image , quotes, shareUrl, type }) => {
         
         <meta property="og:title" content={title} />
         <meta property="og:type" content="News" />
-        <meta property="og:url" content={ogurl} />
+        <meta property="og:url" content="https://job-frontend-main.vercel.app" />
         <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
       </Head>
     </div>
