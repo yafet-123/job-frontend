e4d1569fd6598d094573fc5f34f75eacb9a54140
yafet-123 <yafetaddisu123@gmail.com>
2023-05-06 10:52:12 -0700

    editing the main header

--- components/common/MainHeader.js
+++ components/common/MainHeader.js
@@ -9,12 +9,18 @@ export const MainHeader = ({ title, image , quotes, shareUrl, type }) => {
     <div>
       <Head>
         <title>{title}</title>
-        <meta charSet="utf-8" />
-
-        
-        <meta property="og:title" content={title} />
-        <meta property="og:type" content={type} />
-        <meta property="og:url" content={ogurl} />
+        <meta property="og:url" content={`https://job-frontend-main.vercel.app${shareUrl}`}/>
+        <meta property="og:type" content="website" />
+        <meta property="fb:app_id" content="1233665570615472" />
+        <meta
+          property="og:title"
+          content={title}
+        />
+        <meta name="twitter:card" content={quotes} />
+        <meta
+          property="og:description"
+          content={quotes}
+        />
         <meta property="og:image" content={ogImage} />
       </Head>
     </div>
