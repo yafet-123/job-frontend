64e617ab2f704066cc3058f4eec7f07f2960d4d5
yafet-123 <yafetaddisu123@gmail.com>
2023-05-10 15:03:09 -0700

    editing the meta tag icon

--- components/common/MainHeader.js
+++ components/common/MainHeader.js
@@ -6,21 +6,22 @@ export const MainHeader = ({ title, image , quotes, shareUrl, type, news }) => {
     <div>
       <Head>
         <title>{title}</title>
+        <meta name="description" content="HuluNeger is one the most online recruitment provider in ethiopia, 
+          The website advertises jobs across a wide range of job types by different employers, 
+          inlcuding private, local, international, who are hiring in ethiopia." 
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo1.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/logo1.png">
+
         <meta property="og:url" content={`https://job-frontend-main.vercel.app${shareUrl}`}/>
         <meta property="og:type" content="website" />
         <meta property="fb:app_id" content="1233665570615472" />
-        <meta name="description" content="HuluNeger is one the most online recruitment provider in ethiopia, 
-          The website advertises jobs across a wide range of job types by different employers, 
-          inlcuding private, local, international, who are hiring in ethiopia.">
-        <meta
-          property="og:title"
-          content={title}
-        />
+        <meta property="og:title" content={title} />
         <meta name="twitter:card" content={quotes} />
-        <meta
-          property="og:description"
-          content={quotes}
-        />
+        <meta property="og:description" content={quotes} />
         <meta property="og:image" content={ogImageUrl} />
         <meta property="og:image:secure_url" content={ogImageUrl} />
         <meta property="og:image:width" content="400" />
