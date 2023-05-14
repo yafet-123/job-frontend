0c888254678522c170a66082ba8e660d0c43a12d
yafet-123 <yafetaddisu123@gmail.com>
2023-05-14 11:16:11 -0700

    editing the ai search page

--- components/AiSearch/Hero.js
+++ components/AiSearch/Hero.js
@@ -8,10 +8,10 @@ export const Hero = ({
     <div className="flex justify-center items-center lg:px-20" >
       <div className="flex flex-col gap-8 font-bold text-center md:pt-10">
         <div>
-          <h1 className="text-3xl lg:text-5xl font-bold text-[#009688]  lg:mb-2">{title}</h1>
+          <h1 className="text-3xl lg:text-5xl font-bold text-[#009688] lg:mb-2 hover:text-[#009688]">{title}</h1>
         </div>
         <div>
-          <p className="text-black dark:text-white text-md md:text-xl m-4 font-normal !leading-9">{subtitle}</p>
+          <p className="text-black dark:text-white text-md md:text-xl m-4 font-normal !leading-9 hover:text-[#009688]">{subtitle}</p>
         </div>
       </div>
     </div>
