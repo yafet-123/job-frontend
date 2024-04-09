fe88f679a8116502657d0b19c2672252247d4709
yafet-123 <yafetaddisu123@gmail.com>
2024-04-08 14:34:13 -1200

    edited the admin page auth page

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { FaFacebookF, FaLinkedinIn, FaTwitter, FaYoutube } from "react-icons/fa";
+import { FaFacebookF, FaPhone,FaLinkedinIn, FaTwitter, FaYoutube } from "react-icons/fa";
 import { useRouter } from "next/router";
+import { MdEmail } from "react-icons/md";
 export function Footer() {
   const socialMediaLinks = [
     { path: <FaFacebookF size={20} /> },
@@ -42,8 +43,8 @@ export function Footer() {
           </div>
 
           <p className="text-md lg:text-xl text-white w-full text-center lg:text-left">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elitadipisicing
-            elit.
+            Hulu Neger is a platform dedicated to posting various content, including job applications, blogs, and more. It serves as a comprehensive 
+            hub for users to engage with informative and diverse materials
           </p>
           <div className="flex justify-center items-center space-x-4">
             {socialMediaLinks.map((paths, index) => {
@@ -69,20 +70,42 @@ export function Footer() {
 
         <div className="flex flex-col space-y-2 text-white justify-center items-center">
           <h1 className="text-lg lg:text-2xl font-bold">Contact Info</h1>
-          <p href="#" className="text-md lg:text-lg hover:text-orange-500">
-            Jobs finder
-          </p>
-          <p className="text-md lg:text-lg hover:text-orange-500">+251-934-73-10-38</p>
-          <p className="text-md lg:text-lg hover:text-orange-500">+251-934-73-10-38</p>
-          <Link href="yafetaddisu123@gmail.com">
-            <a className="text-sm lg:text-lg hover:text-orange-500">
-              yafetaddisu123@gmail.com
-            </a>
+          <Link
+            target="_blank"
+            className="flex flex-row items-center gap-2 hover:text-gray-300"
+            href={`tel:${+251934731038}`}
+          >
+            <span className="flex items-center">
+              <FaPhone />{" "}
+              <p className="ml-2 md:ml-3 cursor-pointer">+251-934-73-10-38</p>
+            </span>
+          </Link>
+          <Link
+            target="_blank"
+            className="flex flex-row items-center gap-2 hover:text-gray-300"
+            href={`tel:${+251912843692}`}
+          >
+            <span className="flex items-center">
+              <FaPhone />{" "}
+              <p className="ml-2 md:ml-3 cursor-pointer">+251-912-84-36-92</p>
+            </span>
+          </Link>
+          <Link
+            target="_blank"
+            className="flex flex-row items-center gap-2 hover:text-gray-300"
+            href="mailto:yafetaddisu123@gmail.com"
+          >
+            <span className="flex items-center">
+              <MdEmail />{" "}
+              <p className="ml-2 md:ml-3 cursor-pointer">
+                yafetaddisu123@gmail.com
+              </p>
+            </span>
           </Link>
         </div>
       </div>
       <div className="flex justify-between items-center text-white h-full lg:h-8 mx-3 lg:mx-10">
-        <div className="texl-normal text-sm lg:text-lg md:block">&copy;Hulu 2022.</div>
+        <div className="texl-normal text-sm lg:text-lg md:block">&copy;Hulu Media 2024.</div>
 
         <div className="texl-normal text-sm lg:text-lg md:block">Powered by Hulu Media</div>
       </div>
