43b3c21e215456268ae6df3a96ad9176c45c6ebe
yafet-123 <yafetaddisu123@gmail.com>
2024-07-28 07:00:51 -1200

    edited the website

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
 
-const dev = process.env.NODE_ENV !== "production";
+const dev = false;
 const hostname =
-  process.env.NODE_ENV !== "production"
-    ? "localhost"
-    : "axum.com";
+  process.env.NODE_ENV !== 'production' ? 'localhost' : 'helenzeray.com';
+console.log(hostname)
 const port = process.env.PORT || 4789;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
@@ -20,20 +19,20 @@ app.prepare().then(() => {
       const parsedUrl = parse(req.url, true);
       const { pathname, query } = parsedUrl;
 
-      if (pathname === "/a") {
-        await app.render(req, res, "/a", query);
-      } else if (pathname === "/b") {
-        await app.render(req, res, "/b", query);
+      if (pathname === '/a') {
+        await app.render(req, res, '/a', query);
+      } else if (pathname === '/b') {
+        await app.render(req, res, '/b', query);
       } else {
         await handle(req, res, parsedUrl);
       }
     } catch (err) {
-      console.error("Error occurred handling", req.url, err);
+      console.error('Error occurred handling', req.url, err);
       res.statusCode = 500;
-      res.end("internal server error");
+      res.end('internal server error');
     }
   }).listen(port, (err) => {
     if (err) throw err;
-    // console.log(`> Ready on http://${hostname}:${port}`);
+    console.log(`> Ready on http://${hostname}:${port}`);
   });
 });
